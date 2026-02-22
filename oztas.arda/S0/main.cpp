651bdc0dd47462ea421f8f1d00a1772ede2fe9f7
Arda Boran Öztas <oztas269@gmail.com>
2026-02-22 22:57:34 +0300

    S0 update

--- oztas.arda/S0/main.cpp
+++ oztas.arda/S0/main.cpp
@@ -1,21 +1,7 @@
-#include <boost/test/unit_test.hpp>
-#include <sstream>
+#include <iostream>
 #include "credentials.hpp"
 
-BOOST_AUTO_TEST_SUITE(credentials_suite)
-
-BOOST_AUTO_TEST_CASE(credentials_test)
+int main()
 {
-  std::ostringstream out;
-  oztas::out_credentials(out);
-  BOOST_TEST(out.str() == "oztas.arda");
+  oztas::out_credentials(std::cout) << "\n";
 }
-
-BOOST_AUTO_TEST_CASE(yacredentials_test)
-{
-  std::ostringstream out;
-  oztas::out_credentials(out);
-  BOOST_TEST(out.str() == "oztas.arda");
-}
-
-BOOST_AUTO_TEST_SUITE_END()
