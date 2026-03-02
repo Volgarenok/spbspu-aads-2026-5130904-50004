35c7236d74e36df68e6c5d821879147a0beaaec4
isaacgreenfield <ivtim708@gmail.com>
2026-03-02 20:23:42 +0300

    Baby steps
    (can't yet figure the task :( )

--- ivanov.timur/S1/List.h
+++ ivanov.timur/S1/List.h
@@ -6,16 +6,20 @@ template <class T> class List;
 template <class T>
 class LIter {
   friend class List<T>;
+  List<T>* that;
 
-}
+
+};
 
 template <class T>
 class LCIter {
   friend class List<T>;
-}
+  List<T>* that;
+};
 
 template <class T>
 class List {
-
-}
+  T data;
+  List<T>* next = nullptr;
+};
 #endif
