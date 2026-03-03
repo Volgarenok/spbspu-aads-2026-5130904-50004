dd0ead7654b47dad94c65177e5b3cdc753373e72
isaacgreenfield <ivtim708@gmail.com>
2026-03-03 17:41:35 +0300

    Realized most of the 8) idea
    for List<T> (soon for LIter, LCIter)

--- ivanov.timur/S1/List.h
+++ ivanov.timur/S1/List.h
@@ -22,4 +22,54 @@ class List {
   T data;
   List<T>* next = nullptr;
 };
+
+template <class T>
+LIter<T> getFirstIter(List<T>* head) {
+  return new LIter<T>(head);
+}
+
+template <class T>
+LIter<T> getLast(List<T>* head) {
+  List<T>* tmp = head;
+  while (tmp->next != nullptr) tmp = tmp->next;
+  return new LIter<T>(tmp);
+}
+
+template <class T>
+List<T>* addHead(List<T>* head, T data) {
+  List<T> tmp = new List<T>(data, head);
+  return tmp;
+}
+
+template <class T>
+void addLast(List<T>* head, T data) {
+  while (head->next != nullptr) head = head->next;
+  head->next = new List<T>*(data);
+}
+
+template <class T>
+List<T>* deleteHead(List<T>* head) {
+  List<T>* tmp = head->next;
+  delete head;
+  return tmp;
+}
+
+template <class T>
+void deleteLast(List<T>* head) {
+  while (head->next != nullptr) head = head->next;
+  delete head;
+}
+
+template <class T>
+void clear(List<T>* head) {
+  while (head->next != nullptr) head = deleteHead(head);
+  delete head;
+}
+
+template <class T>
+List<T>* copy(List<T>* thisHead) {
+  List<T>* newHead = thisHead;
+  List<T>* tmp = newHead;
+  return thisHead;
+}
 #endif
