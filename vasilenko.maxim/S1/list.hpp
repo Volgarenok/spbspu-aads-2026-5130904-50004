308cd0152a62579f781d7abcf3448dfd9df9c06d
Madmaxvas <maxmadvas@gmail.com>
2026-03-04 23:58:27 +0300

    Fith commit

--- vasilenko.maxim/S1/list.hpp
+++ vasilenko.maxim/S1/list.hpp
@@ -3,40 +3,93 @@
 
 namespace vasilenko_maxim {
 
-template<class T>
+template< class T > class LIter;
+template< class T > class LCIter;
+
+template< class T >
 class List {
+public:
+  List();
+  ~List();
+
+  void pushBack(const T& value);
+  void clear();
+  bool isEmpty() const;
+
+  T& front();
+  T& back();
+
+  LIter< T > begin();
+  LIter< T > end();
+  LCIter< T > begin() const;
+  LCIter< T > end() const;
+
 private:
   struct Node {
     T data_;
     Node *prev_, *next_;
-    Node(const T& data, Node* p, Node* n) : data_(data), prev_(p), next_(n) {}
+    Node(const T& data, Node* p, Node* n);
   };
 
   Node *head_, *tail_;
 
-public:
-  List() : head_(nullptr), tail_(nullptr) {}
-
-  bool isEmpty() const { return head_ == nullptr; }
-
-  void pushBack(const T& value) {
-    Node* newNode = new Node(value, tail_, nullptr);
-    if (tail_) tail_->next_ = newNode;
-    else head_ = newNode;
-    tail_ = newNode;
-  }
-
-  void clear() {
-    while (head_) {
-      Node* temp = head_;
-      head_ = head_->next_;
-      delete temp;
-    }
-    tail_ = nullptr;
-  }
-
-  ~List() { clear(); }
+  friend class LIter< T >;
+  friend class LCIter< T >;
 };
 
+template< class T >
+class LIter {
+public:
+  LIter(typename List< T >::Node* node);
+
+  T& operator*();
+  LIter& operator++();
+  bool operator==(const LIter& other) const;
+  bool operator!=(const LIter& other) const;
+
+private:
+  typename List< T >::Node* node_;
+};
+
+template< class T >
+List< T >::Node::Node(const T& data, Node* p, Node* n) :
+  data_(data), prev_(p), next_(n)
+{}
+
+template< class T >
+List< T >::List() : head_(nullptr), tail_(nullptr)
+{}
+
+template< class T >
+T& List< T >::front() { return head_->data_; }
+
+template< class T >
+T& List< T >::back() { return tail_->data_; }
+
+template< class T >
+LIter< T > List< T >::begin() { return LIter< T >(head_); }
+
+template< class T >
+LIter< T > List< T >::end() { return LIter< T >(nullptr); }
+
+template< class T >
+LIter< T >::LIter(typename List< T >::Node* node) : node_(node)
+{}
+
+template< class T >
+T& LIter< T >::operator*() { return node_->data_; }
+
+template< class T >
+LIter< T >& LIter< T >::operator++() {
+  node_ = node_->next_;
+  return *this;
 }
+
+template< class T >
+bool LIter< T >::operator!=(const LIter& other) const {
+  return node_ != other.node_;
+}
+
+}
+
 #endif
