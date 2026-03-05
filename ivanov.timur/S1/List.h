68f26eec7afd1da4cd4d093db7b22c6cdc744513
isaacgreenfield <ivtim708@gmail.com>
2026-03-05 13:21:28 +0300

    downsize

--- ivanov.timur/S1/List.h
+++ ivanov.timur/S1/List.h
@@ -1,6 +1,5 @@
 #ifndef LIST_H
 #define LIST_H
-#include <stdexcept>
 
 template <class T> class LIter;
 template <class T> class LCIter;
@@ -9,7 +8,7 @@ template <class T>
 class Iter {
 public:
   class List {
-  public:
+  protected:
     friend class Iter<T>;
     T data;
     List* next;
@@ -82,60 +81,4 @@ public:
     return that->data;
   }
 };
-
-template <class T>
-Iter<T> getLast(Iter<T> head) noexcept{
-  while (head.that->next != nullptr) head.that = head.that->next;
-  return head;
-}
-
-template <class T>
-Iter<T> getPrevLast(Iter<T> head) {
-  if (head.that == nullptr || head.that->next == nullptr) throw std::logic_error("cannot find prev last");
-  while (head.that->next->next != nullptr) head.that = head.that->next;
-  return head;
-}
-
-template <class T>
-void addLast(Iter<T> head, T data) {
-  if (head.that == nullptr) head = new Iter<T>::List(data, nullptr);
-  else {
-    head = getLast(head);
-    head.that->next = new Iter<T>::List(data, nullptr);
-  }
-}
-
-template <class T>
-void deleteLast(Iter<T> head) noexcept{
-  delete getPrevLast(head).that->next;
-  getPrevLast(head).that->next = nullptr;
-}
-
-template <class T>
-void clear(Iter<T> head) noexcept{
-  if (head.that != nullptr) {
-    while (head.that->next != nullptr) {
-      head.that->deleteSelf();
-    }
-  }
-  delete head.that;
-}
-
-template <class T>
-Iter<T> copy(Iter<T> thisHead) {
-  Iter<T> newHead = Iter<T>(thisHead.that);
-  if (thisHead.that == nullptr) throw std::logic_error("cannot use nullptr state");
-  try {
-    Iter<T> tmp = newHead; // i dont think thats correct
-    while (thisHead.that->next != nullptr) { //does this work?
-      tmp.that->next = new Iter<T>::List(thisHead.that->next->data, nullptr);
-      tmp.that = tmp.that->next;
-      thisHead.that = thisHead.that->next;
-    } //do I need to think about last elem?
-  } catch (...) { //which error does this throw?
-    clear(newHead); //newHead is an object, not sure whether that would be correct
-    throw std::logic_error("copy faliure fatal");
-  }
-  return newHead;
-}
 #endif
