ce6bc230a960fe7acf9e0559f93522a5cc52de1e
isaacgreenfield <ivtim708@gmail.com>
2026-03-06 14:43:39 +0300

    retrial iterator (not split between L/LC)

--- ivanov.timur/S1/List.h
+++ ivanov.timur/S1/List.h
@@ -3,9 +3,43 @@
 
 #include <stdexcept>
 
+template <class T> class List;
+
 template <class T>
 class Iter {
-  friend class List;
+    friend class List<T>;
+
+public:
+    Iter() noexcept : ptr_(nullptr) {}
+    Iter(const Iter&) noexcept = default;
+    Iter(Iter&&) noexcept = default;
+    ~Iter() = default;
+    Iter& operator=(const Iter&) noexcept = default;
+    Iter& operator=(Iter&&) noexcept = default;
+
+
+    T& operator*() const noexcept {
+        return ptr_->data_;
+    }
+    T* operator->() const noexcept {
+        return &(ptr_->data_);
+    }
+
+    Iter& operator++() noexcept {
+        ptr_ = ptr_->next_;
+        return *this;
+    }
+
+    bool operator==(const Iter& other) const noexcept {
+        return ptr_ == other.ptr_;
+    }
+    bool operator!=(const Iter& other) const noexcept {
+        return ptr_ != other.ptr_;
+    }
+
+private:
+    typename List<T>::Elem* ptr_;
+    explicit Iter(typename List<T>::Elem* p) noexcept : ptr_(p) {}
 };
 
 template <class T>
