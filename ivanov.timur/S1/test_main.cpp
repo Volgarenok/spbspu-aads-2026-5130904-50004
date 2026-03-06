091188fab9e831aee2c118feb202d7a00ecfa27a
isaacgreenfield <ivtim708@gmail.com>
2026-03-06 22:07:50 +0300

    fixing boost tests AGAIN

--- ivanov.timur/S1/test_main.cpp
+++ ivanov.timur/S1/test_main.cpp
@@ -1,66 +1,32 @@
 #if defined(__GNUC__) || defined(__clang__)
 __attribute__((weak))
 #endif
-#define BOOST_TEST_MODULE projectTests
+#define BOOST_TEST_MODULE ListTests
 #include <boost/test/included/unit_test.hpp>
 #include "List.h"
 
 using namespace ivanov;
 
-BOOST_AUTO_TEST_SUITE(ListTests)
+BOOST_AUTO_TEST_CASE(push)
+{
+  List<int> list;
+  BOOST_CHECK(list.empty());
 
-BOOST_AUTO_TEST_CASE(DefaultConstructorAndCapacity) {
-    List<int> lst;
-    BOOST_CHECK(lst.empty());
-    BOOST_CHECK_EQUAL(lst.size(), 0);
+  list.push_back(10);
+  list.push_front(5);
+  BOOST_CHECK_EQUAL(list.front(), 5);
+  BOOST_CHECK_EQUAL(list.back(), 10);
+
 }
 
-BOOST_AUTO_TEST_CASE(PushAndPopFront) {
-    List<int> lst;
-    lst.push_front(10);
-    BOOST_CHECK(!lst.empty());
-    BOOST_CHECK_EQUAL(lst.size(), 1);
-    BOOST_CHECK_EQUAL(lst.front(), 10);
-    BOOST_CHECK_EQUAL(lst.back(), 10);
+BOOST_AUTO_TEST_CASE(pop)
+{
+  List<int> list;
+  list.push_back(10);
+  list.push_front(5);
 
-    lst.push_front(20);
-    BOOST_CHECK_EQUAL(lst.size(), 2);
-    BOOST_CHECK_EQUAL(lst.front(), 20);
-    BOOST_CHECK_EQUAL(lst.back(), 10);
-
-    lst.pop_front();
-    BOOST_CHECK_EQUAL(lst.size(), 1);
-    BOOST_CHECK_EQUAL(lst.front(), 10);
-
-    lst.pop_front();
-    BOOST_CHECK(lst.empty());
-
-    lst.pop_front();
-    BOOST_CHECK(lst.empty());
+  list.pop_front();
+  BOOST_CHECK_EQUAL(list.front(), 10);
+  list.pop_back();
+  BOOST_CHECK(list.empty());
 }
-
-BOOST_AUTO_TEST_CASE(PushAndPopBack) {
-    List<int> lst;
-    lst.push_back(10);
-    BOOST_CHECK(!lst.empty());
-    BOOST_CHECK_EQUAL(lst.size(), 1);
-    BOOST_CHECK_EQUAL(lst.front(), 10);
-    BOOST_CHECK_EQUAL(lst.back(), 10);
-
-    lst.push_back(20);
-    BOOST_CHECK_EQUAL(lst.size(), 2);
-    BOOST_CHECK_EQUAL(lst.front(), 10);
-    BOOST_CHECK_EQUAL(lst.back(), 20);
-
-    lst.pop_back();
-    BOOST_CHECK_EQUAL(lst.size(), 1);
-    BOOST_CHECK_EQUAL(lst.back(), 10);
-
-    lst.pop_back();
-    BOOST_CHECK(lst.empty());
-
-    lst.pop_back();
-    BOOST_CHECK(lst.empty());
-}
-
-BOOST_AUTO_TEST_SUITE_END()
