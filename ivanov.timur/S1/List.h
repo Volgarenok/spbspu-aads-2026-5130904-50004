ee0f4fea0940a4e4241dfc86e115c971fdc92284
isaacgreenfield <ivtim708@gmail.com>
2026-03-06 15:01:35 +0300

    Created signature for List
    +
    
    created some methods to rule-5 and xtra in it

--- ivanov.timur/S1/List.h
+++ ivanov.timur/S1/List.h
@@ -42,13 +42,97 @@ private:
     explicit Iter(typename List<T>::Elem* p) noexcept : ptr_(p) {}
 };
 
-template <class T>
+template <typename T>
 class List {
-  struct Elem {
-    T data_;
-    Elem* next_;
-  };
+private:
+    struct Elem {
+        T data;
+        Elem* next;
+        explicit Elem(const T& val, Elem* nxt = nullptr) : data(val), next(nxt) {}
+        explicit Elem(T&& val, Elem* nxt = nullptr) : data(std::move(val)), next(nxt) {}
+    };
 
+    Elem* head;
+    Elem* tail;
+    size_t sz;
+
+public:
+    List() noexcept : head(nullptr), tail(nullptr), sz(0) {}
+
+    ~List() {
+        clear();
+    }
+
+    List(const List& other) : head(nullptr), tail(nullptr), sz(0) {
+        for (Elem* curr = other.head; curr != nullptr; curr = curr->next) {
+            push_back(curr->data);
+        }
+    }
+
+    List(List&& other) noexcept : head(other.head), tail(other.tail), sz(other.sz) {
+        other.head = nullptr;
+        other.tail = nullptr;
+        other.sz = 0;
+    }
+
+    List& operator=(const List& other) {
+        if (this != &other) {
+            List tmp(other);
+            swap(tmp);
+        }
+        return *this;
+    }
+
+    List& operator=(List&& other) noexcept {
+        if (this != &other) {
+            clear();
+            head = other.head;
+            tail = other.tail;
+            sz   = other.sz;
+            other.head = nullptr;
+            other.tail = nullptr;
+            other.sz = 0;
+        }
+        return *this;
+    }
+
+    void swap(List& other) noexcept {
+        Elem *tmp_head = head;
+        Elem *tmp_tail = tail;
+        size_t tmp_sz = sz;
+        head = other.head;
+        tail = other.tail;
+        sz = other.sz;
+        other.head = tmp_head;
+        other.tail = tmp_tail;
+        other.sz = tmp_sz;
+    }
+
+    Iter<T> begin() noexcept {
+        return Iter<T>(head);
+    };
+    Iter<T> end() noexcept {
+        return Iter<T>(tail);
+    };
+
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+
+    T& front();
+    const T& front() const;
+    T& back();
+    const T& back() const;
+
+    void push_front(const T& value);
+    void push_front(T&& value);
+    void pop_front();
+    void push_back(const T& value);
+    void push_back(T&& value);
+    void pop_back();
+    Iter<T> insert_after(Iter<T> pos, const T& value);
+    Iter<T> insert_after(Iter<T> pos, T&& value);
+    Iter<T> erase_after(Iter<T> pos);
+    void clear();
 };
 
 #endif
