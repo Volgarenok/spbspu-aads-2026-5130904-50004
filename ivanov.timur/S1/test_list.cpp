884408c4ece6353c25827de8b91981e2ede8dacd
isaacgreenfield <ivtim708@gmail.com>
2026-03-06 19:42:51 +0300

    zhong-li-ruya kodom

--- ivanov.timur/S1/test_list.cpp
+++ ivanov.timur/S1/test_list.cpp
@@ -1,37 +1,63 @@
-#define BOOST_TEST_MODULE AppLogicTests
-#include <boost/test/included/unit_test.hpp>
-#include <sstream>
+#define BOOST_TEST_MODULE ivanov_list_test
+#include <boost/test/unit_test.hpp>
+#include "List.h"
 #include <string>
-#include "code.h"
-BOOST_AUTO_TEST_CASE(test_given_example) {
-  std::stringstream in("first 1 1 1\nsecond 2 2 2 2\nthird\nfourth 4 4\n");
-  std::stringstream out;
-  std::stringstream err;
-  int result = run(in, out, err);
-  BOOST_CHECK_EQUAL(result, 0);
-  std::string expected_out =
-    "first second third fourth\n"
-    "1 2 4\n"
-    "1 2 4\n"
-    "1 2\n"
-    "2\n"
-    "7 7 3 2\n";
-  BOOST_CHECK_EQUAL(out.str(), expected_out);
-  BOOST_CHECK_EQUAL(err.str(), "");
+#include <stdexcept>
+#include <limits>
+
+using namespace ivanov;
+
+template <typename T>
+bool check_list_content(const List<T>& lst, const std::initializer_list<T>& expected) {
+  if (lst.size() != expected.size()) return false;
+  auto it = lst.cbegin();
+  auto exp_it = expected.begin();
+  while (it != lst.cend() && exp_it != expected.end()) {
+    if (*it != *exp_it) return false;
+    ++it;
+    ++exp_it;
+  }
+  return true;
 }
-BOOST_AUTO_TEST_CASE(test_empty_input) {
-  std::stringstream in("");
-  std::stringstream out;
-  std::stringstream err;
-  int result = run(in, out, err);
-  BOOST_CHECK_EQUAL(result, 0);
-  BOOST_CHECK_EQUAL(out.str(), "");
+
+BOOST_AUTO_TEST_SUITE(list_tests)
+
+BOOST_AUTO_TEST_CASE(default_constructor) {
+  List<int> lst;
+  BOOST_CHECK(lst.empty());
+  BOOST_CHECK_EQUAL(lst.size(), 0u);
+  BOOST_CHECK(lst.begin() == lst.end());
+  BOOST_CHECK(lst.cbegin() == lst.cend());
 }
-BOOST_AUTO_TEST_CASE(test_numeric_limit_overflow) {
-  std::stringstream in("a 2147483647\nb 1\n");
-  std::stringstream out;
-  std::stringstream err;
-  int result = run(in, out, err);
-  BOOST_CHECK_EQUAL(result, 1);
-  BOOST_CHECK_EQUAL(err.str(), "Error during sum processing\n");
+
+BOOST_AUTO_TEST_CASE(push_front_lvalue) {
+  List<int> lst;
+  int a = 10, b = 20;
+  lst.push_front(a);
+  BOOST_CHECK_EQUAL(lst.size(), 1u);
+  BOOST_CHECK_EQUAL(lst.front(), 10);
+  BOOST_CHECK_EQUAL(lst.back(), 10);
+
+  lst.push_front(b);
+  BOOST_CHECK_EQUAL(lst.size(), 2u);
+  BOOST_CHECK_EQUAL(lst.front(), 20);
+  BOOST_CHECK_EQUAL(lst.back(), 10);
+
+  BOOST_CHECK(check_list_content(lst, {20, 10}));
 }
+
+BOOST_AUTO_TEST_CASE(push_front_rvalue) {
+  List<std::string> lst;
+  lst.push_front(std::string("world"));
+  lst.push_front(std::string("hello"));
+  BOOST_CHECK_EQUAL(lst.size(), 2u);
+  BOOST_CHECK_EQUAL(lst.front(), "hello");
+  BOOST_CHECK_EQUAL(lst.back(), "world");
+}
+
+BOOST_AUTO_TEST_CASE(sum_overflow_throws) {
+  int a = std::numeric_limits<int>::max();
+  BOOST_CHECK_THROW(sum(a, 1), std::logic_error);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
