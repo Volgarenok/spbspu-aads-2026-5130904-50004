285859a9613be431209aca95dd4b60c75b26cd7d
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-07 19:27:13 +0300

    feat(List, ListIterators): add namespace

--- alekseev.ivan/S1/List.h
+++ alekseev.ivan/S1/List.h
@@ -1,65 +1,67 @@
 #ifndef LIST_H
 #define LIST_H
 
-template< class T >
-struct List {
-  T data;
-  List< T > * next;
-};
+namespace alekseev {
+  template< class T >
+  struct List {
+    T data;
+    List< T > * next;
+  };
 
-template< class T >
-List< T > * fake(List< T > * head)
-{
-  List< T > * fake_node = static_cast< List< T > * >(::operator new(sizeof(List< T > *)));
-  fake_node->next = head;
-  head->next = fake_node;
-  return fake_node;
-}
-
-template< class T >
-List< T > * fake()
-{
-  List< T > * fake_node = static_cast< List< T > * >(::operator new(sizeof(List< T > *)));
-  fake_node->next = fake_node;
-  return fake_node;
-}
-
-template< class T >
-List< T > * rmfake(List< T > * fake_node)
-{
-  List< T > * head = fake_node->next;
-  ::operator delete(fake_node);
-  return head;
-}
-
-template< class T >
-List< T > insert_after(List< T > * element, const T & value)
-{
-  List< T > * new_node = new List< T >();
-  new_node->data = value;
-  new_node->next = element->next;
-  element->next = new_node;
-  return new_node;
-}
-
-template< class T >
-List< T > * erase_after(List< T > * element)
-{
-  List< T > * tmp = element->next->next;
-  delete element->next;
-  element->next = tmp;
-  return element->next;
-}
-
-template< class T >
-List< T > * clear(List< T > * start, List< T > * end)
-{
-  while (start != end) {
-    List< T > * tmp = start->next;
-    delete start;
-    start = tmp;
+  template< class T >
+  List< T > * fake(List< T > * head)
+  {
+    List< T > * fake_node = static_cast< List< T > * >(::operator new(sizeof(List< T > *)));
+    fake_node->next = head;
+    head->next = fake_node;
+    return fake_node;
   }
-  return start;
+
+  template< class T >
+  List< T > * fake()
+  {
+    List< T > * fake_node = static_cast< List< T > * >(::operator new(sizeof(List< T > *)));
+    fake_node->next = fake_node;
+    return fake_node;
+  }
+
+  template< class T >
+  List< T > * rmfake(List< T > * fake_node)
+  {
+    List< T > * head = fake_node->next;
+    ::operator delete(fake_node);
+    return head;
+  }
+
+  template< class T >
+  List< T > insert_after(List< T > * element, const T & value)
+  {
+    List< T > * new_node = new List< T >();
+    new_node->data = value;
+    new_node->next = element->next;
+    element->next = new_node;
+    return new_node;
+  }
+
+  template< class T >
+  List< T > * erase_after(List< T > * element)
+  {
+    List< T > * tmp = element->next->next;
+    delete element->next;
+    element->next = tmp;
+    return element->next;
+  }
+
+  template< class T >
+  List< T > * clear(List< T > * start, List< T > * end)
+  {
+    while (start != end) {
+      List< T > * tmp = start->next;
+      delete start;
+      start = tmp;
+    }
+    return start;
+  }
 }
 
 #endif
--- alekseev.ivan/S1/ListIterators.h
+++ alekseev.ivan/S1/ListIterators.h
@@ -2,175 +2,178 @@
 #define LISTITERATORS_H
 
 #include "List.h"
+#include <memory>
 
-template< class T >
-struct ListIteratorBase {
-  friend class List< T >;
+namespace alekseev {
+  template< class T >
+  struct ListIteratorBase {
+    friend class List< T >;
 
-  ListIteratorBase();
+    ListIteratorBase();
 
-  ListIteratorBase(List< T > * node);
+    ListIteratorBase(List< T > * node);
 
-  ListIteratorBase< T > & operator++();
-  ListIteratorBase< T > operator++(int);
+    ListIteratorBase< T > & operator++();
+    ListIteratorBase< T > operator++(int);
 
-  bool operator==(const ListIteratorBase< T > & other) const;
-  bool operator!=(const ListIteratorBase< T > & other) const;
+    bool operator==(const ListIteratorBase< T > & other) const;
+    bool operator!=(const ListIteratorBase< T > & other) const;
 
-  protected:
-    List< T > * node_;
-};
+    protected:
+      List< T > * node_;
+  };
 
-template< class T >
-ListIteratorBase< T >::ListIteratorBase():
-  node_(nullptr)
-{
-}
-
-template< class T >
-ListIteratorBase< T >::ListIteratorBase(List< T > * node):
-  node_(node)
-{
-}
-
-template< class T >
-ListIteratorBase< T > & ListIteratorBase< T >::operator++()
-{
-  node_ = node_->next;
-  return *this;
-}
-
-template< class T >
-ListIteratorBase< T > ListIteratorBase< T >::operator++(int)
-{
-  ListIteratorBase< T > tmp = *this;
-  ++(*this);
-  return tmp;
-}
-
-template< class T >
-bool ListIteratorBase< T >::operator==(const ListIteratorBase< T > & other) const
-{
-  return node_ == other.node_;
-}
-
-template< class T >
-bool ListIteratorBase< T >::operator!=(const ListIteratorBase< T > & other) const
-{
-  return !(*this == other);
-}
-
-template< class T >
-struct LIter: ListIteratorBase< T > {
-  using ListIteratorBase< T >::ListIteratorBase;
-
-  LIter(const ListIteratorBase< T > & base);
-
-  T & operator*() const;
-  T * operator->() const;
-};
-
-template< class T >
-LIter< T >::LIter(const ListIteratorBase< T > & base):
-  ListIteratorBase< T >(base)
-{
-}
-
-template< class T >
-T & LIter< T >::operator*() const
-{
-  return this->node->data;
-}
-
-template< class T >
-T * LIter< T >::operator->() const
-{
-  return &(this->node->data);
-}
-
-template< class T >
-struct LCIter: ListIteratorBase< T > {
-  using ListIteratorBase< T >::ListIteratorBase;
-
-  LCIter(const LIter< T > & rhs);
-  LCIter(const ListIteratorBase< T > & base);
-
-  const T & operator*() const;
-  const T * operator->() const;
-};
-
-template< class T >
-LCIter< T >::LCIter(const LIter< T > & rhs):
-  ListIteratorBase< T >(rhs)
-{
-}
-
-template< class T >
-LCIter< T >::LCIter(const ListIteratorBase< T > & base):
-  ListIteratorBase< T >(base)
-{
-}
-
-template< class T >
-const T & LCIter< T >::operator*() const
-{
-  return this->node->data;
-}
-
-template< class T >
-const T * LCIter< T >::operator->() const
-{
-  return &(this->node->data);
-}
-
-template< class T >
-LIter< T > begin(List< T > * fake_node)
-{
-  return LIter< T >(fake_node->next);
-}
-
-template< class T >
-LIter< T > before_begin(List< T > * fake_node)
-{
-  return LIter< T >(fake_node);
-}
-
-template< class T >
-LIter< T > end(List< T > * fake_node)
-{
-  List< T > * current = fake_node;
-  while (current->next != fake_node) {
-    current = current->next;
+  template< class T >
+  ListIteratorBase< T >::ListIteratorBase():
+    node_(nullptr)
+  {
   }
-  return LIter< T >(current);
-}
 
-template< class T >
-LCIter< T > begin(const List< T > * fake_node)
-{
-  return LCIter< T >(fake_node->next);
-}
-
-template< class T >
-LCIter< T > before_begin(const List< T > * fake_node)
-{
-  return LCIter< T >(fake_node);
-}
-
-template< class T >
-LCIter< T > end(const List< T > * fake_node)
-{
-  List< T > * current = fake_node;
-  while (current->next != fake_node) {
-    current = current->next;
+  template< class T >
+  ListIteratorBase< T >::ListIteratorBase(List< T > * node):
+    node_(node)
+  {
   }
-  return LCIter< T >(current);
-}
 
-template< class Iter >
-Iter next(Iter it)
-{
-  return ++it;
+  template< class T >
+  ListIteratorBase< T > & ListIteratorBase< T >::operator++()
+  {
+    node_ = node_->next;
+    return *this;
+  }
+
+  template< class T >
+  ListIteratorBase< T > ListIteratorBase< T >::operator++(int)
+  {
+    ListIteratorBase< T > tmp = *this;
+    ++(*this);
+    return tmp;
+  }
+
+  template< class T >
+  bool ListIteratorBase< T >::operator==(const ListIteratorBase< T > & other) const
+  {
+    return node_ == other.node_;
+  }
+
+  template< class T >
+  bool ListIteratorBase< T >::operator!=(const ListIteratorBase< T > & other) const
+  {
+    return !(*this == other);
+  }
+
+  template< class T >
+  struct LIter: ListIteratorBase< T > {
+    using ListIteratorBase< T >::ListIteratorBase;
+
+    LIter(const ListIteratorBase< T > & base);
+
+    T & operator*() const;
+    T * operator->() const;
+  };
+
+  template< class T >
+  LIter< T >::LIter(const ListIteratorBase< T > & base):
+    ListIteratorBase< T >(base)
+  {
+  }
+
+  template< class T >
+  T & LIter< T >::operator*() const
+  {
+    return this->node->data;
+  }
+
+  template< class T >
+  T * LIter< T >::operator->() const
+  {
+    return std::addressof(this->node->data);
+  }
+
+  template< class T >
+  struct LCIter: ListIteratorBase< T > {
+    using ListIteratorBase< T >::ListIteratorBase;
+
+    LCIter(const LIter< T > & rhs);
+    LCIter(const ListIteratorBase< T > & base);
+
+    const T & operator*() const;
+    const T * operator->() const;
+  };
+
+  template< class T >
+  LCIter< T >::LCIter(const LIter< T > & rhs):
+    ListIteratorBase< T >(rhs)
+  {
+  }
+
+  template< class T >
+  LCIter< T >::LCIter(const ListIteratorBase< T > & base):
+    ListIteratorBase< T >(base)
+  {
+  }
+
+  template< class T >
+  const T & LCIter< T >::operator*() const
+  {
+    return this->node->data;
+  }
+
+  template< class T >
+  const T * LCIter< T >::operator->() const
+  {
+    return std::addressof(this->node->data);
+  }
+
+  template< class T >
+  LIter< T > begin(List< T > * fake_node)
+  {
+    return LIter< T >(fake_node->next);
+  }
+
+  template< class T >
+  LIter< T > before_begin(List< T > * fake_node)
+  {
+    return LIter< T >(fake_node);
+  }
+
+  template< class T >
+  LIter< T > end(List< T > * fake_node)
+  {
+    List< T > * current = fake_node;
+    while (current->next != fake_node) {
+      current = current->next;
+    }
+    return LIter< T >(current);
+  }
+
+  template< class T >
+  LCIter< T > begin(const List< T > * fake_node)
+  {
+    return LCIter< T >(fake_node->next);
+  }
+
+  template< class T >
+  LCIter< T > before_begin(const List< T > * fake_node)
+  {
+    return LCIter< T >(fake_node);
+  }
+
+  template< class T >
+  LCIter< T > end(const List< T > * fake_node)
+  {
+    List< T > * current = fake_node;
+    while (current->next != fake_node) {
+      current = current->next;
+    }
+    return LCIter< T >(current);
+  }
+
+  template< class Iter >
+  Iter next(Iter it)
+  {
+    return ++it;
+  }
 }
 
 #endif
