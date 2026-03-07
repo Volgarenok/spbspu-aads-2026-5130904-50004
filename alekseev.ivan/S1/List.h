ca6c455cb5e90ac20b99ff9560e5d69b4aca3221
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-07 16:52:46 +0300

    feat(List): LIter/LCIter base interface init

--- alekseev.ivan/S1/List.h
+++ alekseev.ivan/S1/List.h
@@ -1,9 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
 
-
 template< class T >
-struct  List {
+struct List {
   T data;
   List< T > * next;
 };
@@ -11,13 +10,42 @@ struct  List {
 template< class T >
 struct LIter {
   friend class List< T >;
+  List< T > * node;
 
+  LIter():
+    node(nullptr)
+  {
+  }
+
+  LIter< T > & operator++();
+  LIter< T > operator++(int);
+
+  T & operator*();
+  T & operator->();
+
+  bool operator==(const LIter< T > & other) const;
+  bool operator!=(const LIter< T > & other) const;
 };
 
 template< class T >
 struct LCIter {
   friend class List< T >;
 
+  List< T > * node;
+
+  LIter():
+    node(nullptr)
+  {
+  }
+
+  LIter< T > & operator++();
+  LIter< T > operator++(int);
+
+  T & operator*();
+  T & operator->();
+
+  bool operator==(const LIter< T > & other) const;
+  bool operator!=(const LIter< T > & other) const;
 };
 
 #endif
