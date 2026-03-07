ccb36ce35a113c1c551e44a7c1d420323a52e099
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-07 16:54:41 +0300

    feat(List): add fake

--- alekseev.ivan/S1/List.h
+++ alekseev.ivan/S1/List.h
@@ -48,4 +48,21 @@ struct LCIter {
   bool operator!=(const LIter< T > & other) const;
 };
 
+template< class T >
+List< T > * fake(List< T > * head)
+{
+  List< T > * fake_node = static_cast< List< T > * >(::operator new(sizeof(List< T > *)));
+  fake_node->next = head;
+  head->next = fake_node;
+  return fake_node;
+}
+
+template< class T >
+List< T > * rmfake(List< T > * fake_node)
+{
+  List< T > * head = fake_node->next;
+  ::operator delete(fake_node);
+  return head;
+}
+
 #endif
