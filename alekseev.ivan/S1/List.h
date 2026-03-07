6784780ee4f2073745bbb2be76ed4a07efc44322
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-07 16:59:28 +0300

    feat(List): add fake()

--- alekseev.ivan/S1/List.h
+++ alekseev.ivan/S1/List.h
@@ -58,6 +58,14 @@ List< T > * fake(List< T > * head)
 }
 
 template< class T >
+List< T > * fake()
+{
+  List< T > * fake_node = static_cast< List< T > * >(::operator new(sizeof(List< T > *)));
+  fake_node->next = fake_node;
+  return fake_node;
+}
+
+template< class T >
 List< T > * rmfake(List< T > * fake_node)
 {
   List< T > * head = fake_node->next;
