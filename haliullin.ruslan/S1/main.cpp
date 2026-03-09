05eae711d4a28e5d196e968970691ffa8e2951df
knopkloop <hruslan2007@gmail.com>
2026-03-09 18:53:36 +0300

    begin main, initalize data structures

--- haliullin.ruslan/S1/main.cpp
+++ haliullin.ruslan/S1/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <utility>
+#include "BiList.hpp"
+
+using namespace haliullin;
 
 int main()
 {
-
+   try
+  {
+    BiList<std::pair<std::string, BiList<unsigned long long>>> storage;
+    std::string token;
+    unsigned long long value = 0;
+  }
+  catch(const std::exception &e)
+  {
+    std::cerr << e.what() << "\n";
+    return 1;
+  }
 }
