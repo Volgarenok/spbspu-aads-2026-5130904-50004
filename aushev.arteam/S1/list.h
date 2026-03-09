6c698f2e48d011db0bd7d98d819f0f00559175bb
Ausev.Arteam <seregabest77@gmail.com>
2026-03-09 14:37:51 +0300

    Implement iterator operators

--- aushev.arteam/S1/list.h
+++ aushev.arteam/S1/list.h
@@ -12,6 +12,15 @@ template< class T >
 class LIter {
   friend class List< T >;
 
+public:
+  LIter();
+  LIter& operator++();
+  LIter operator++(int);
+  T& operator*() const;
+  T* operator->() const;
+  bool operator==(const LIter& other) const;
+  bool operator!=(const LIter& other) const;
+
 private:
   struct Node {
     T data;
@@ -23,6 +32,15 @@ private:
 template< class T >
 class LCIter {
   friend class List< T >;
+
+public:
+  LCIter();
+  LCIter& operator++();
+  LCIter operator++(int);
+  const T& operator*() const;
+  const T* operator->() const;
+  bool operator==(const LCIter& other) const;
+  bool operator!=(const LCIter& other) const;
 
 private:
   struct Node {
@@ -66,6 +84,100 @@ private:
   Node* tail_;
   size_t size_;
 };
+
+template< class T >
+LIter< T >::LIter()
+  : node_(nullptr)
+{
+}
+
+template< class T >
+LIter< T >& LIter< T >::operator++()
+{
+  if (node_) {
+    node_ = node_->next;
+  }
+  return *this;
+}
+
+template< class T >
+LIter< T > LIter< T >::operator++(int)
+{
+  LIter tmp = *this;
+  ++(*this);
+  return tmp;
+}
+
+template< class T >
+T& LIter< T >::operator*() const
+{
+  return node_->data;
+}
+
+template< class T >
+T* LIter< T >::operator->() const
+{
+  return &(node_->data);
+}
+
+template< class T >
+bool LIter< T >::operator==(const LIter& other) const
+{
+  return node_ == other.node_;
+}
+
+template< class T >
+bool LIter< T >::operator!=(const LIter& other) const
+{
+  return node_ != other.node_;
+}
+
+template< class T >
+LCIter< T >::LCIter()
+  : node_(nullptr)
+{
+}
+
+template< class T >
+LCIter< T >& LCIter< T >::operator++()
+{
+  if (node_) {
+    node_ = node_->next;
+  }
+  return *this;
+}
+
+template< class T >
+LCIter< T > LCIter< T >::operator++(int)
+{
+  LCIter tmp = *this;
+  ++(*this);
+  return tmp;
+}
+
+template< class T >
+const T& LCIter< T >::operator*() const
+{
+  return node_->data;
+}
+
+template< class T >
+const T* LCIter< T >::operator->() const
+{
+  return &(node_->data);
+}
+
+template< class T >
+bool LCIter< T >::operator==(const LCIter& other) const
+{
+  return node_ == other.node_;
+}
+
+template< class T >
+bool LCIter< T >::operator!=(const LCIter& other) const
+{
+  return node_ != other.node_;
+}
 
 template< class T >
 List< T >::List()
