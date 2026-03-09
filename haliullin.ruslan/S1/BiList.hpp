511052631bceb98cf6235bde8c204d624517dd06
knopkloop <hruslan2007@gmail.com>
2026-03-09 17:54:23 +0300

    added BiList methods implementation

--- haliullin.ruslan/S1/BiList.hpp
+++ haliullin.ruslan/S1/BiList.hpp
@@ -1,5 +1,7 @@
 #ifndef BILIST_HPP
 #define BILIST_HPP
+#include <iostream>
+#include <utility>
 
 namespace haliullin
 {
@@ -23,14 +25,27 @@ namespace haliullin
       prev(nullptr),
       next(nullptr)
     {}
+
+    Node(const T & value, Node * p, Node * n) :
+      val(value),
+      prev(p),
+      next(n)
+    {}
+
+    Node(T && value, Node * p, Node * n) :
+      val(std::move(value)),
+      prev(p),
+      next(n)
+    {}
   };
+
 
 //----------LIter----------
   template< class T >
   class LIter
   {
-    friend class BiList< T >;
-    Node< T > * cur;
+    friend class BiList<T>;
+    Node<T> * cur;
     explicit LIter(Node<T> * node) noexcept :
       cur(node)
     {}
@@ -51,7 +66,7 @@ namespace haliullin
       return cur == other.cur;
     }
 
-    bool operator!=(const LIter & other) const noexcept;
+    bool operator!=(const LIter & other) const noexcept
     {
       return !(*this == other);
     }
@@ -66,7 +81,7 @@ namespace haliullin
       return &(cur->val);
     }
 
-    LIter & operator++() noexcept;
+    LIter & operator++() noexcept
     {
       cur = cur->next;
       return *this;
@@ -79,7 +94,7 @@ namespace haliullin
       return tmp;
     }
 
-    LIter & operator--() noexcept;
+    LIter & operator--() noexcept
     {
       cur = cur->prev;
       return *this;
@@ -92,6 +107,7 @@ namespace haliullin
       return tmp;
     }
   };
+
 
 //----------LCIter----------
   template< class T >
@@ -109,6 +125,8 @@ namespace haliullin
     {}
 
     LCIter(const LIter<T> & other) noexcept :
+      cur(other.cur)
+    {}
 
     LCIter(const LCIter & other) noexcept = default;
     LCIter(LCIter && other) noexcept = default;
@@ -137,13 +155,13 @@ namespace haliullin
       return &(cur->val);
     }
 
-    LCIter & operator++() noexcept;
+    LCIter & operator++() noexcept
     {
       cur = cur->next;
       return *this;
     }
 
-    LCIter operator++(int) noexcept;
+    LCIter operator++(int) noexcept
     {
       LCIter tmp(*this);
       cur = cur->next;
@@ -164,6 +182,7 @@ namespace haliullin
     }
   };
 
+
 //----------BiList----------
   template< class T >
   class BiList
@@ -177,26 +196,27 @@ namespace haliullin
       head(nullptr),
       size(0)
     {}
+
     BiList(const BiList & other) :
       head(nullptr),
       size(0)
     {
-      if (other.isempty())
+      if (other.is_empty())
       {
         return;
       }
 
-      Node< T > * cur = other.head;
-      Node< T > * first = new Node< T >(current->val);
+      Node<T> * current = other.head;
+      Node<T> * first = new Node<T>(current->val);
       head = first;
       size = 1;
 
       current = current->next;
-      Node< T > * prev = first;
+      Node<T> * prev = first;
 
       while (current != other.head)
       {
-        Node< T > * newNode = new Node< T >(current->val);
+        Node<T> * newNode = new Node<T>(current->val);
         prev->next = newNode;
         newNode->prev = prev;
         prev = newNode;
@@ -209,12 +229,11 @@ namespace haliullin
     }
 
     BiList(BiList && other) noexcept :
-      head(other.head);
+      head(other.head),
       size(other.size)
     {
       other.head = nullptr;
       other.size = 0;
-
     }
 
     ~BiList()
@@ -232,7 +251,7 @@ namespace haliullin
       return *this;
     }
 
-    BiList & operator=(BiList && other)
+    BiList & operator=(BiList && other) noexcept
     {
       if (this != &other)
       {
@@ -245,40 +264,351 @@ namespace haliullin
       return *this;
     }
 
-    void clear() noexcept;
+    bool is_empty() const noexcept
+    {
+      return size == 0;
+    }
 
-    bool is_empty() const noexcept;
+    size_t getsize() const noexcept
+    {
+      return size;
+    }
 
-    size_t getsize() const noexcept;
+    T & front()
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+      return head->val;
+    }
 
-    T & front() noexcept;
-    const T & front() const noexcept;
+    const T & front() const
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+      return head->val;
+    }
 
-    T & back() noexcept;
-    const T & back() const noexcept;
+    T & back()
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+      return head->prev->val;
+    }
 
-    LIter<T> begin();
-    LCIter<T> cbegin() const;
+    const T & back() const
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+      return head->prev->val;
+    }
 
-    LIter<T> end();
-    LCIter<T> cend() const;
+    LIter<T> begin() noexcept
+    {
+      return LIter<T>(head);
+    }
 
-    void push_front(const T & value);
-    void push_front(T && value);
-    void push_back(const T & value);
-    void push_back(T && value);
+    LCIter<T> begin() const noexcept
+    {
+      return LCIter<T>(head);
+    }
 
-    LIter<T> insert_before(LIter<T> pos, const T & value);
-    LIter<T> insert_before(LIter<T> pos, T && value);
-    LIter<T> insert_after(LIter<T> pos, const T & value);
-    LIter<T> insert_after(LIter<T> pos, T && value);
+    LCIter<T> cbegin() const noexcept
+    {
+      return LCIter<T>(head);
+    }
 
-    void pop_front() noexcept;
-    void pop_back() noexcept;
+    LIter<T> end() noexcept
+    {
+      return LIter<T>(nullptr);
+    }
 
-    LIter<T> erase(LIter<T> pos) noexcept;
+    LCIter<T> end() const noexcept
+    {
+      return LCIter<T>(nullptr);
+    }
 
-    void swap(BiList & other) noexcept;
+    LCIter<T> cend() const noexcept
+    {
+      return LCIter<T>(nullptr);
+    }
+
+    void push_front(const T & value)
+    {
+      Node<T> * newNode = new Node<T>(value);
+      if(is_empty())
+      {
+        head = newNode;
+        head->next = head;
+        head->prev = head;
+      }
+      else
+      {
+        newNode->next = head;
+        newNode->prev = head->prev;
+        head->prev->next = newNode;
+        head->prev = newNode;
+        head = newNode;
+      }
+      ++size;
+    }
+
+    void push_front(T && value)
+    {
+      Node<T> * newNode = new Node<T>(std::move(value));
+      if(is_empty())
+      {
+        head = newNode;
+        head->next = head;
+        head->prev = head;
+      }
+      else
+      {
+        newNode->next = head;
+        newNode->prev = head->prev;
+        head->prev->next = newNode;
+        head->prev = newNode;
+        head = newNode;
+      }
+      ++size;
+    }
+
+    void push_back(const T & value)
+    {
+      if(is_empty())
+      {
+        push_front(value);
+      }
+      else
+      {
+        Node<T> * newNode = new Node<T>(value);
+        Node<T> * last = head->prev;
+
+        newNode->next = head;
+        newNode->prev = last;
+
+        last->next = newNode;
+        head->prev = newNode;
+
+        ++size;
+      }
+    }
+
+    void push_back(T && value)
+    {
+      if(is_empty())
+      {
+        push_front(std::move(value));
+      }
+      else
+      {
+        Node<T> * newNode = new Node<T>(std::move(value));
+        Node<T> * last = head->prev;
+
+        newNode->next = head;
+        newNode->prev = last;
+
+        last->next = newNode;
+        head->prev = newNode;
+
+        ++size;
+      }
+    }
+
+    LIter<T> insert_after(LIter<T> pos, const T & value)
+    {
+      if (is_empty())
+      {
+        push_front(value);
+        return begin();
+      }
+
+      if (pos.cur == nullptr)
+      {
+        push_front(value);
+        return begin();
+      }
+
+      Node<T> * newNode = new Node<T>(value, pos.cur, pos.cur->next);
+      pos.cur->next->prev = newNode;
+      pos.cur->next = newNode;
+
+      if (pos.cur == head->prev)
+      {
+        head->prev = newNode;
+      }
+
+      ++size;
+
+      return LIter<T>(newNode);
+    }
+
+    LIter<T> insert_after(LIter<T> pos, T && value)
+    {
+      if (is_empty())
+      {
+        push_front(std::move(value));
+        return begin();
+      }
+
+      if (pos.cur == nullptr)
+      {
+        push_front(std::move(value));
+        return begin();
+      }
+
+      Node<T> * newNode = new Node<T>(std::move(value), pos.cur, pos.cur->next);
+      pos.cur->next->prev = newNode;
+      pos.cur->next = newNode;
+
+      if (pos.cur == head->prev)
+      {
+        head->prev = newNode;
+      }
+
+      ++size;
+
+      return LIter<T>(newNode);
+    }
+
+    LIter<T> insert_before(LIter<T> pos, const T & value)
+    {
+      if (pos.cur == nullptr)
+      {
+        push_back(value);
+        return LIter<T>(head->prev);
+      }
+
+      LIter<T> prev(pos.cur->prev);
+      return insert_after(prev, value);
+    }
+
+    LIter<T> insert_before(LIter<T> pos, T && value)
+    {
+      if (pos.cur == nullptr)
+      {
+        push_back(std::move(value));
+        return LIter<T>(head->prev);
+      }
+
+      LIter<T> prev(pos.cur->prev);
+      return insert_after(prev, std::move(value));
+    }
+
+    void pop_front()
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+      else if (getsize() == 1)
+      {
+        delete head;
+        head = nullptr;
+        size = 0;
+      }
+      else
+      {
+        Node<T> * first = head;
+        head->prev->next = head->next;
+        head->next->prev = head->prev;
+        head = head->next;
+        delete first;
+        --size;
+      }
+    }
+
+    void pop_back()
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+      else if (getsize() == 1)
+      {
+        delete head;
+        head = nullptr;
+        size = 0;
+      }
+      else
+      {
+        Node<T> * last = head->prev;
+        last->prev->next = head;
+        head->prev = last->prev;
+        delete last;
+        --size;
+      }
+    }
+
+    LIter<T> erase(LIter<T> pos)
+    {
+      if (is_empty())
+      {
+        throw std::runtime_error("Empty list");
+      }
+
+      if (pos.cur == nullptr)
+      {
+        throw std::runtime_error("Cannot erase end iterator");
+      }
+
+      if (getsize() == 1)
+      {
+        delete pos.cur;
+        head = nullptr;
+        size = 0;
+        return end();
+      }
+
+      Node<T> * toDelete = pos.cur;
+      Node<T> * nextNode = toDelete->next;
+
+      if (toDelete == head)
+      {
+        pop_front();
+        return begin();
+      }
+      else if (toDelete == head->prev)
+      {
+        pop_back();
+        return end();
+      }
+      else
+      {
+        toDelete->prev->next = toDelete->next;
+        toDelete->next->prev = toDelete->prev;
+        delete toDelete;
+        --size;
+        return LIter<T>(nextNode);
+      }
+    }
+
+    void clear() noexcept
+    {
+      while(!is_empty())
+      {
+        pop_front();
+      }
+    }
+
+    void swap(BiList & other) noexcept
+    {
+      Node<T> * tmp_head = head;
+      size_t tmp_size = size;
+
+      head = other.head;
+      size = other.size;
+
+      other.head = tmp_head;
+      other.size = tmp_size;
+    }
   };
 }
 
