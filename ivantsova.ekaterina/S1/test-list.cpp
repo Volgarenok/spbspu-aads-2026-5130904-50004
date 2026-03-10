df127dacbd28e7a5387296771547bb13c5ea5ddc
ekaterinaivanc <kataivancova7@gmail.com>
2026-03-10 18:28:11 +0300

    rename

--- ivantsova.ekaterina/S1/test-list.cpp
+++ ivantsova.ekaterina/S1/test-list.cpp
@@ -1,7 +1,3 @@
-#if defined(__GNUC__) || defined(__clang__)
-__attribute__((weak)) int main(int argc, char* argv[]);
-#endif
-
 #define BOOST_TEST_MODULE ListTests
 #include <boost/test/included/unit_test.hpp>
 #include "list.hpp"
