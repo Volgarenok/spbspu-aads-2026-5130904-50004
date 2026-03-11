e6d3a2c590ee64812228d102a1c5e9caff8684d5
emiraydgn <emiraydogan03@gmail.com>
2026-03-12 02:53:04 +0300

    input reading

--- aydogan.emir/S1/logic.cpp
+++ aydogan.emir/S1/logic.cpp
@@ -9,6 +9,38 @@ namespace aydogan
 
 void readInput(std::istream& in, SequenceList& data)
 {
+    std::string name;
+  auto seqTail = data.beforeBegin();
+
+  while (in >> name)
+  {
+    NumberList numbers;
+    auto numTail = numbers.beforeBegin();
+
+    while (true)
+    {
+      int c = in.peek();
+
+      if (c == '\n' || c == EOF)
+      {
+        break;
+      }
+
+      unsigned long long value = 0;
+
+      if (!(in >> value))
+      {
+        throw std::overflow_error("Overflow");
+      }
+
+      numTail = numbers.insertAfter(numTail, value);
+    }
+
+    seqTail = data.insertAfter(
+      seqTail,
+      std::make_pair(name, numbers)
+    );
+  }
 }
 
 int runProgram(
