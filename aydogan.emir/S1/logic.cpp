9c81524d50916552b1422b4e98aa6c191bab2299
emiraydgn <emiraydogan03@gmail.com>
2026-03-12 02:50:25 +0300

    logic start

---
+++ aydogan.emir/S1/logic.cpp
@@ -0,0 +1,23 @@
+#include "logic.hpp"
+
+#include <iostream>
+#include <limits>
+#include <stdexcept>
+
+namespace aydogan
+{
+
+void readInput(std::istream& in, SequenceList& data)
+{
+}
+
+int runProgram(
+  const SequenceList& data,
+  std::ostream& out,
+  std::ostream& err
+)
+{
+  return 0;
+}
+
+}
