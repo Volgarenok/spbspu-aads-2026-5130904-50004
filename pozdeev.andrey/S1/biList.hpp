76598efe826eef31e5d5f839fb517a15552cd37b
ryouta25 <avp22.07@mail.ru>
2026-03-14 19:52:20 +0300

    add Node and BiList skeleton

--- pozdeev.andrey/S1/biList.hpp
+++ pozdeev.andrey/S1/biList.hpp
@@ -1,4 +1,51 @@
 #ifndef POZDEEV_BILIST_HPP
 #define POZDEEV_BILIST_HPP
 
+#include <cstddef>
+#include <stdexcept>
+#include <utility>
+
+namespace pozdeev {
+
+template<class T>
+class BiList;
+
+template<class T>
+class Node {
+public:
+  T val_;
+  Node<T>* prev_;
+  Node<T>* next_;
+
+  Node(const T& value) :
+    val_(value),
+    prev_(nullptr),
+    next_(nullptr)
+  {
+  }
+
+  Node(T&& value) :
+    val_(std::move(value)),
+    prev_(nullptr),
+    next_(nullptr)
+  {
+  }
+
+  Node(const T& value, Node* p, Node* n) :
+    val_(value),
+    prev_(p),
+    next_(n)
+  {
+  }
+
+  Node(T&& value, Node* p, Node* n) :
+    val_(std::move(value)),
+    prev_(p),
+    next_(n)
+  {
+  }
+};
+
+}
+
 #endif
