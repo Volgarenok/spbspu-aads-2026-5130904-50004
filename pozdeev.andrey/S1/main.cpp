6449aaae2d6b1f25de97c1af01d7db6e7ef68cff
ryouta25 <avp22.07@mail.ru>
2026-03-14 21:07:19 +0300

    цикл чтения данных и структура хранения данных

--- pozdeev.andrey/S1/main.cpp
+++ pozdeev.andrey/S1/main.cpp
@@ -1,2 +1,43 @@
+#include <iostream>
+#include <limits>
+#include <string>
+#include <utility>
+
+#include "biList.hpp"
+
 int main()
-{}
+{
+  pozdeev::BiList<std::string> listNames;
+  pozdeev::BiList<pozdeev::BiList<int>> listValues;
+  std::string currentName;
+
+  while (std::cin >> currentName) {
+    pozdeev::BiList<int> numbers;
+
+    int ch = std::cin.get();
+    while (ch != std::char_traits<char>::eof() && ch != '\n') {
+      if (std::isspace(ch)) {
+        ch = std::cin.get();
+        continue;
+      }
+      std::cin.unget();
+
+      int num = 0;
+      std::cin >> num;
+      if (std::cin.fail()) {
+        std::cerr << "Input error\n";
+        return 1;
+      }
+      numbers.pushBack(num);
+      ch = std::cin.get();
+    }
+
+    listNames.pushBack(currentName);
+    listValues.pushBack(std::move(numbers));
+  }
+
+  if (listNames.isEmpty()) {
+    std::cout << "0\n";
+    return 0;
+  }
+}
