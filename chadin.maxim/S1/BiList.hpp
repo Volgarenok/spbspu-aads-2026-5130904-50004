80a08d4233e5e4e65ffca9650f0ce163a3d348ea
maxchadin <maxchadin080418@gmail.com>
2026-03-17 21:50:40 +0300

    add class BiList

--- chadin.maxim/S1/BiList.hpp
+++ chadin.maxim/S1/BiList.hpp
@@ -201,6 +201,245 @@ namespace chadin
       head_(head)
     {}
   };
+
+template< class T >
+  class BiList
+  {
+  public:
+    BiList() noexcept :
+      head_(nullptr),
+      size_(0)
+    {}
+
+    BiList(const BiList& other) :
+      head_(nullptr),
+      size_(0)
+    {
+      if (!other.isEmpty())
+      {
+        Node< T >* curr = other.head_;
+        do
+        {
+          pushBack(curr->val);
+          curr = curr->next;
+        }
+        while (curr != other.head_);
+      }
+    }
+
+    BiList(BiList&& other) noexcept :
+      head_(other.head_),
+      size_(other.size_)
+    {
+      other.head_ = nullptr;
+      other.size_ = 0;
+    }
+
+    ~BiList()
+    {
+      clear();
+    }
+
+    BiList& operator=(const BiList& other)
+    {
+      if (this != &other)
+      {
+        BiList tmp(other);
+        swap(tmp);
+      }
+      return *this;
+    }
+
+    BiList& operator=(BiList&& other) noexcept
+    {
+      if (this != &other)
+      {
+        clear();
+        head_ = other.head_;
+        size_ = other.size_;
+        other.head_ = nullptr;
+        other.size_ = 0;
+      }
+      return *this;
+    }
+
+    void swap(BiList& other) noexcept
+    {
+      std::swap(head_, other.head_);
+      std::swap(size_, other.size_);
+    }
+
+    bool isEmpty() const noexcept
+    {
+      return size_ == 0;
+    }
+
+    size_t getSize() const noexcept
+    {
+      return size_;
+    }
+
+    T& front()
+    {
+      if (isEmpty())
+      {
+        throw std::out_of_range("List is empty");
+      }
+      return head_->val;
+    }
+
+    const T& front() const
+    {
+      if (isEmpty())
+      {
+        throw std::out_of_range("List is empty");
+      }
+      return head_->val;
+    }
+
+    T& back()
+    {
+      if (isEmpty())
+      {
+        throw std::out_of_range("List is empty");
+      }
+      return head_->prev->val;
+    }
+
+    const T& back() const
+    {
+      if (isEmpty())
+      {
+        throw std::out_of_range("List is empty");
+      }
+      return head_->prev->val;
+    }
+
+    LIter< T > begin() noexcept
+    {
+      return LIter< T >(head_, head_);
+    }
+
+    LIter< T > end() noexcept
+    {
+      return LIter< T >(nullptr, head_);
+    }
+
+    LCIter< T > cbegin() const noexcept
+    {
+      return LCIter< T >(head_, head_);
+    }
+
+    LCIter< T > cend() const noexcept
+    {
+      return LCIter< T >(nullptr, head_);
+    }
+
+    void pushFront(const T& value)
+    {
+      Node< T >* newNode = new Node< T >(value);
+      if (isEmpty())
+      {
+        head_ = newNode;
+        head_->next = head_;
+        head_->prev = head_;
+      }
+      else
+      {
+        Node< T >* tail = head_->prev;
+        newNode->next = head_;
+        newNode->prev = tail;
+        tail->next = newNode;
+        head_->prev = newNode;
+        head_ = newNode;
+      }
+      ++size_;
+    }
+
+    void pushBack(const T& value)
+    {
+      pushFront(value);
+      head_ = head_->next;
+    }
+
+    void popFront()
+    {
+      if (isEmpty())
+      {
+        return;
+      }
+      if (size_ == 1)
+      {
+        delete head_;
+        head_ = nullptr;
+      }
+      else
+      {
+        Node< T >* oldHead = head_;
+        Node< T >* tail = head_->prev;
+        head_ = head_->next;
+        head_->prev = tail;
+        tail->next = head_;
+        delete oldHead;
+      }
+      --size_;
+    }
+
+    void popBack()
+    {
+      if (isEmpty())
+      {
+        return;
+      }
+      if (size_ == 1)
+      {
+        popFront();
+      }
+      else
+      {
+        Node< T >* tail = head_->prev;
+        tail->prev->next = head_;
+        head_->prev = tail->prev;
+        delete tail;
+        --size_;
+      }
+    }
+
+    void clear() noexcept
+    {
+      while (!isEmpty())
+      {
+        popFront();
+      }
+    }
+
+    LIter< T > insert(LIter< T > pos, const T& value)
+    {
+      if (pos == begin())
+      {
+        pushFront(value);
+        return begin();
+      }
+      if (pos == end())
+      {
+        pushBack(value);
+        LIter< T > res = end();
+        return --res;
+      }
+      Node< T >* curr = pos.node_;
+      Node< T >* newNode = new Node< T >(value);
+      newNode->prev = curr->prev;
+      newNode->next = curr;
+      curr->prev->next = newNode;
+      curr->prev = newNode;
+      ++size_;
+      return LIter< T >(newNode, head_);
+    }
+
+  private:
+    Node< T >* head_;
+    size_t size_;
+  };
 }
 
 #endif
