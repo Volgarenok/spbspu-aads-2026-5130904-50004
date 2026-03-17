5a072c612c02c68d71c79a561e8d8712daad57df
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-17 20:31:15 +0300

    fix(test-list): fix tests

--- alekseev.ivan/S1/test-list.cpp
+++ alekseev.ivan/S1/test-list.cpp
@@ -6,14 +6,14 @@ BOOST_AUTO_TEST_CASE (fake)
 {
   alekseev::List< int > * list = alekseev::fake< int >();
   BOOST_CHECK_EQUAL(list->next, list);
-  alekseev::insert_after(list, 10);
-  BOOST_CHECK_EQUAL(list->next->data, 10);
-  alekseev::insert_after(list, 20);
-  BOOST_CHECK_EQUAL(list->next->next->data, 20);
+  list = alekseev::insert_after(list, 10);
+  BOOST_CHECK_EQUAL(list->data, 10);
+  list = alekseev::insert_after(list, 20);
+  BOOST_CHECK_EQUAL(list->data, 20);
   BOOST_CHECK_EQUAL(list->next->next->next, list);
+  list = list->next;
   alekseev::erase_after(list);
   BOOST_CHECK_EQUAL(list->next->data, 20);
   alekseev::clear(list->next, list);
-  BOOST_CHECK_EQUAL(list->next, list);
   alekseev::rmfake(list);
 }
