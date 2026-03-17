6cb330475734adf5da34c5a65e8b69ed4367e541
maxchadin <maxchadin080418@gmail.com>
2026-03-17 21:02:22 +0300

    add Node structure

--- chadin.maxim/S1/BiList.hpp
+++ chadin.maxim/S1/BiList.hpp
@@ -7,6 +7,26 @@
 namespace chadin
 {
   template< class T >
+  struct Node
+  {
+    T val;
+    Node* prev;
+    Node* next;
+
+    Node(const T& value) :
+      val(value),
+      prev(nullptr),
+      next(nullptr)
+    {}
+
+    Node(T&& value) :
+      val(std::move(value)),
+      prev(nullptr),
+      next(nullptr)
+    {}
+  };
+
+  template< class T >
   class BiList;
 }
 
