ad6ce41e43a66735747f3e806115231247c7984e
maxchadin <maxchadin080418@gmail.com>
2026-03-17 21:12:30 +0300

    add class LIter

--- chadin.maxim/S1/BiList.hpp
+++ chadin.maxim/S1/BiList.hpp
@@ -28,6 +28,91 @@ namespace chadin
 
   template< class T >
   class BiList;
+
+  template< class T >
+  class LIter
+  {
+  public:
+    friend class BiList< T >;
+
+    LIter() noexcept :
+      node_(nullptr),
+      head_(nullptr)
+    {}
+
+    T& operator*() const
+    {
+      return node_->val;
+    }
+
+    T* operator->() const
+    {
+      return &(node_->val);
+    }
+
+    LIter& operator++() noexcept
+    {
+      if (node_)
+      {
+        node_ = node_->next;
+        if (node_ == head_)
+        {
+          node_ = nullptr;
+        }
+      }
+      return *this;
+    }
+
+    LIter operator++(int) noexcept
+    {
+      LIter tmp(*this);
+      ++(*this);
+      return tmp;
+    }
+
+    LIter& operator--() noexcept
+    {
+      if (node_ == nullptr)
+      {
+        if (head_)
+        {
+          node_ = head_->prev;
+        }
+      }
+      else
+      {
+        if (node_ == head_)
+        {
+          node_ = nullptr;
+        }
+        else
+        {
+          node_ = node_->prev;
+        }
+      }
+      return *this;
+    }
+
+    bool operator==(const LIter& other) const noexcept
+    {
+      return node_ == other.node_;
+    }
+
+    bool operator!=(const LIter& other) const noexcept
+    {
+      return !(*this == other);
+    }
+
+  private:
+    Node< T >* node_;
+    Node< T >* head_;
+
+    LIter(Node< T >* node, Node< T >* head) noexcept :
+      node_(node),
+      head_(head)
+    {}
+  };
+
 }
 
 #endif
