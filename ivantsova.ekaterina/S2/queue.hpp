30cb5bfc6f7d1843e10c10a89fc17c6f664ea059
ekaterinaivanc <kataivancova7@gmail.com>
2026-03-21 16:43:33 +0300

    add queue

---
+++ ivantsova.ekaterina/S2/queue.hpp
@@ -0,0 +1,71 @@
+#ifndef QUEUE_HPP
+#define QUEUE_HPP
+
+#include "../common/list.hpp"
+
+namespace ivantsova{
+
+  template < class T >
+  class Queue {
+  private:
+    List<T> data_;
+
+  public:
+    void push(const T& value);
+    T pop();
+    T& front();
+    T& back();
+    const T& back() const;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    void clear();
+  };
+
+  template < class T >
+  void Stack<T>::push(const T& value) {
+    data_.push_front(value);
+  }
+
+  template < class T >
+  T Stack<T>::pop() {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    T value = std::move(data_.front());
+    data_.pop_front();
+    return value;
+  }
+
+  template < class T >
+  T& Stack<T>::top() {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return data_.front();
+  }
+
+  template < class T >
+  const T& Stack<T>::top() const {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return data_.front();
+  }
+
+  template < class T >
+  bool Stack<T>::empty() const noexcept {
+    return data_.empty();
+  }
+
+  template < class T >
+  size_t Stack<T>::size() const noexcept {
+    return data_.size();
+  }
+
+  template < class T >
+  void Stack<T>::clear() {
+    return data_.clear();
+  }
+}
+
+#endif
