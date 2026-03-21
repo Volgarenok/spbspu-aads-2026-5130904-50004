c14b05d5592f750307a10fa630a4746c44ffcff9
ekaterinaivanc <kataivancova7@gmail.com>
2026-03-21 16:28:26 +0300

    implementation stack

--- ivantsova.ekaterina/S2/stack.hpp
+++ ivantsova.ekaterina/S2/stack.hpp
@@ -11,14 +11,60 @@ namespace ivantsova{
     List<T> data_;
 
   public:
-    Stack() = default;
-    void push(const T& value) {}
-    T pop() {}
-    T& top() {}
-    const T& top() const {}
-    bool empty() const {}
-    size_t size() const {}
-    void clear() {}
+    void push(const T& value);
+    T pop();
+    T& top();
+    const T& top() const;
+    bool empty() const noexcept;
+    size_t size() const noexcept;
+    void clear();
   };
+
+  template < class T >
+  void Stack<T>::push(const T& value) {
+    data_.push_front(value);
+  }
+
+  template < class T >
+  T Stack<T>::pop() {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    T value = std::move(data_.front());
+    data_.pop_front();
+    return value;
+  }
+
+  template < class T >
+  T& Stack<T>::top() {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return data_.front();
+  }
+
+  template < class T >
+  const T& Stack<T>::top() const {
+    if (empty()) {
+      throw std::out_of_range("Stack is empty");
+    }
+    return data_.front();
+  }
+
+  template < class T >
+  bool Stack<T>::empty() const noexcept {
+    return data_.empty();
+  }
+
+  template < class T >
+  size_t Stack<T>::size() const noexcept {
+    return data_.size();
+  }
+
+  template < class T >
+  void Stack<T>::clear() {
+    return data_.clear();
+  }
 }
+
 #endif
