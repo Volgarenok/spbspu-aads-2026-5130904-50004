5c4fe69537fd2ce8d1a46a8dbf3aef02e157c99c
FShaaM <zinovevas135@gmail.com>
2026-03-23 16:54:24 +0300

    OPYT'

--- zinoviev.alexander/S2/test-lcm.cpp
+++ zinoviev.alexander/S2/test-lcm.cpp
@@ -2,20 +2,27 @@
 #include <boost/test/unit_test.hpp>
 #include <cstdlib>
 
-long long gcd(long long a, long long b) {
-    while (b) { long long t = b; b = a % b; a = t; }
-    return a;
+long long gcd(long long a, long long b)
+{
+  while (b)
+  {
+    long long t = b; b = a % b; a = t;
+  }
+  return a;
 }
 
-long long lcm(long long a, long long b) {
-    a = std::llabs(a); b = std::llabs(b);
-    long long g = gcd(a, b);
-    return g ? (a / g) * b : 0;
+long long lcm(long long a, long long b)
+{
+  a = std::llabs(a); b = std::llabs(b);
+  long long g = gcd(a, b);
+
+  return g ? (a / g) * b : 0;
 }
 
-BOOST_AUTO_TEST_CASE(testLcm) {
-    BOOST_CHECK_EQUAL(lcm(2, 3), 6);
-    BOOST_CHECK_EQUAL(lcm(0, 5), 0);
-    BOOST_CHECK_EQUAL(lcm(-4, 6), 12);
-    BOOST_CHECK_EQUAL(lcm(12, 18), 36);
+BOOST_AUTO_TEST_CASE(testLcm)
+{
+  BOOST_CHECK_EQUAL(lcm(2, 3), 6);
+  BOOST_CHECK_EQUAL(lcm(0, 5), 0);
+  BOOST_CHECK_EQUAL(lcm(-4, 6), 12);
+  BOOST_CHECK_EQUAL(lcm(12, 18), 36);
 }
