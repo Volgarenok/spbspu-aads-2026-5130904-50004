d15460fa9b0af95c9a6cbff265d87ce2abd16237
FShaaM <zinovevas135@gmail.com>
2026-03-23 15:41:55 +0300

    Started the main(...)

--- zinoviev.alexander/S2/main.cpp
+++ zinoviev.alexander/S2/main.cpp
@@ -1,4 +1,23 @@
 #include "evaluate.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
 
-int main()
-{}
+int main(int argc, char* argv[])
+{
+  using namespace zinoviev;
+
+  std::ifstream input;
+  std::istream* in = &std::cin;
+
+  if (argc == 2)
+  {
+    input.open(argv[1]);
+    if (!input)
+    {
+      std::cerr << "Cannot open file\n";
+      return 1;
+    }
+    in = &input;
+  }
+}
