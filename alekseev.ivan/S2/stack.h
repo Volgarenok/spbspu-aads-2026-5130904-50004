76a530cb4a1484f9f6f40bad8cb80a8cf1bacf56
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-24 18:13:35 +0300

    feat(Stack): add rule of five in Stack interface

--- alekseev.ivan/S2/stack.h
+++ alekseev.ivan/S2/stack.h
@@ -9,6 +9,13 @@ namespace alekseev {
     List< T > fake_node_;
     size_t size_;
 
+    Stack();
+    ~Stack();
+    Stack( const Stack & );
+    Stack & operator=( const Stack & );
+    Stack( Stack && );
+    Stack & operator=( Stack && );
+
     void push(T & rhs);
     void pop();
     T & top();
