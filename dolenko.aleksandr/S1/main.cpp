9ea8020db8ed53ef21bc3017d00378c2cae9a6b7
dolenko.aleksandr <dolenko.ayu@edu.spbstu.ru>
2026-03-24 12:24:55 +0300

    add input parsing without stringstream

--- dolenko.aleksandr/S1/main.cpp
+++ dolenko.aleksandr/S1/main.cpp
@@ -1,3 +1,62 @@
-int main(){
+#include <cctype>
+#include <iostream>
+#include <string>
+#include <utility>
 
-};
+#include "BiList.hpp"
+
+namespace
+{
+	using Sequence = std::pair< std::string, dolenko::BiList< long long > >;
+
+	dolenko::BiList< Sequence > readSequences(std::istream& in)
+	{
+		dolenko::BiList< Sequence > sequences;
+
+		std::string name;
+		in >> std::skipws;
+		while (in >> name)
+		{
+			dolenko::BiList< long long > numbers;
+
+			in >> std::noskipws;
+			while (true)
+			{
+				char ch = 0;
+				if (!in.get(ch))
+				{
+					break;
+				}
+				if (ch == '\r')
+				{
+					continue;
+				}
+				if (ch == '\n')
+				{
+					break;
+				}
+				if (std::isspace(static_cast< unsigned char >(ch)))
+				{
+					continue;
+				}
+
+				in.unget();
+				long long value = 0;
+				in >> value;
+				numbers.push_back(value);
+			}
+
+			in >> std::skipws;
+			sequences.push_back(Sequence(name, numbers));
+		}
+
+		return sequences;
+	}
+}
+
+int main()
+{
+	auto sequences = readSequences(std::cin);
+	(void)sequences;
+	return 0;
+}
