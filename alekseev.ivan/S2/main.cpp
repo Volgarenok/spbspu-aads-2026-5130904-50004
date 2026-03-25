703e0752c5e2238e6de0b3a12269f0ca12695a4e
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-25 18:54:46 +0300

    feat(main): beta version of main

--- alekseev.ivan/S2/main.cpp
+++ alekseev.ivan/S2/main.cpp
@@ -1,3 +1,27 @@
-int main()
+#include <iostream>
+#include <fstream>
+#include "expressions_processing.h"
+#include "stack.h"
+
+int main(int argc, char ** argv)
 {
+  std::ifstream file;
+  if (argc == 2) {
+    file.open(argv[1]);
+  }
+  std::istream & input = argc == 2 ? file : std::cin;
+
+  alekseev::Stack< int > res;
+  while (!input.eof()) {
+    std::string line;
+    std::getline(input, line);
+    res.push(alekseev::count_from_string(line));
+  }
+
+  std::cout << res.top();
+  res.pop();
+  while (!res.empty()) {
+    std::cout << " " << res.top();
+    res.pop();
+  }
 }
