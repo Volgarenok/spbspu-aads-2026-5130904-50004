62f5ce76b10bee75e4fe66bc06e96814ee49037f
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-25 20:02:55 +0300

    feat(safety_math): add sub

--- alekseev.ivan/safety_math.cpp
+++ alekseev.ivan/safety_math.cpp
@@ -15,3 +15,17 @@ int alekseev::sum(int a, int b)
   }
   return a + b;
 }
+
+int alekseev::sub(int a, int b)
+{
+  if (a > 0 && b < 0) {
+    if (a > MAX + b) {
+      throw std::overflow_error("Sub overflow");
+    }
+  } else if (a < 0 && b > 0) {
+    if (a < MIN + b) {
+      throw std::underflow_error("Sub underflow");
+    }
+  }
+  return a - b;
+}
