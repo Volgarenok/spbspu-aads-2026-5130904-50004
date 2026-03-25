fe5c0d5a0ec91065354796a2b5f784747c93cf2c
emiraydgn <emiraydogan03@gmail.com>
2026-03-25 12:12:00 +0300

    added front method and pop front

--- aydogan.emir/S2/list.hpp
+++ aydogan.emir/S2/list.hpp
@@ -14,9 +14,28 @@ bool empty() const noexcept
 {
   return head_ == nullptr;
 }
+T& front()
+{
+  if (empty())
+  {
+    throw std::out_of_range("List is empty");
+  }
+  return head_->data;
+}
 void push_front(const T& value)
 {
   head_ = new Node(value, head_);
+}
+void pop_front()
+{
+  if (empty())
+  {
+    throw std::out_of_range("List is empty");
+  }
+
+  Node* tmp = head_;
+  head_ = head_->next;
+  delete tmp;
 }
   private:
   struct Node
