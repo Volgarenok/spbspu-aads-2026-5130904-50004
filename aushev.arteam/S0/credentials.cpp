882316f101b20c43d94751af8a7524adf0313935
woken1 <82443038+woken1@users.noreply.github.com>
2026-03-25 21:39:15 +0300

    Revert "Aushev.arteam/s0"

--- aushev.arteam/S0/credentials.cpp
+++
@@ -1,7 +0,0 @@
-#include "credentials.hpp"
-#include <iostream>
-
-std::ostream & aushev::out_credentials(std::ostream & out)
-{
-  return out << "aushev.arteam";
-}
--- aushev.arteam/S0/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "credentials.hpp"
-
-int main()
-{
-  aushev::out_credentials(std::cout) << "\n";
-}
