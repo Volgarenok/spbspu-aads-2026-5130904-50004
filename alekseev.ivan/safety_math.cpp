c47bc587bf954d2819c0c40bd3de0e658499cab5
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-25 20:00:16 +0300

    feat(safety_math): add sum

--- alekseev.ivan/safety_math.cpp
+++ alekseev.ivan/safety_math.cpp
@@ -1,1 +1,17 @@
 #include "safety_math.h"
+
+#include <stdexcept>
+
+int alekseev::sum(int a, int b)
+{
+  if (a > 0 && b > 0) {
+    if (a > MAX - b) {
+      throw std::overflow_error("Sum overflow");
+    }
+  } else if (a < 0 && b < 0) {
+    if (a < MIN - b) {
+      throw std::underflow_error("Sum underflow");
+    }
+  }
+  return a + b;
+}
--- alekseev.ivan/safety_math.h
+++ alekseev.ivan/safety_math.h
@@ -1,13 +1,18 @@
 #ifndef SAFETY_MATH_H
 #define SAFETY_MATH_H
 
+#include <limits>
+
 namespace alekseev {
-  int add(int a, int b);
+  int sum(int a, int b);
   int sub(int a, int b);
   int mul(int a, int b);
   int div(int a, int b);
   int mod(int a, int b);
   int flip(int a);
+
+  const int MAX = std::numeric_limits<int>::max();
+  const int MIN = std::numeric_limits<int>::min();
 }
 
 #endif
