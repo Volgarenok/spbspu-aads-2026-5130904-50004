fa663dffc7ae18a1b73e7b0c92a132dbc4681763
emiraydgn <emiraydogan03@gmail.com>
2026-03-25 13:18:11 +0300

    final fixes on queue

--- aydogan.emir/S2/queue.hpp
+++ aydogan.emir/S2/queue.hpp
@@ -2,8 +2,8 @@
 #define QUEUE_HPP
 
 #include "list.hpp"
-#include <utility>
 #include <stdexcept>
+#include <utility>
 
 namespace aydogan
 {
@@ -11,75 +11,125 @@ namespace aydogan
   class Queue
   {
   public:
-    Queue()
-  data_(),
-  tail_(data_.beforeBegin())
-{}
+    Queue():
+      data_(),
+      tail_(data_.beforeBegin())
+    {}
 
-bool empty() const noexcept
-{
-  return data_.empty();
-}
+    Queue(const Queue& other):
+      data_(other.data_),
+      tail_(data_.beforeBegin())
+    {
+      rebuildTail();
+    }
 
-void push(const T& rhs)
-{
-  if (empty())
-  {
-    tail_ = data_.insertAfter(data_.beforeBegin(), rhs);
-  }
-  else
-  {
-    tail_ = data_.insertAfter(tail_, rhs);
-  }
-}
+    Queue(Queue&& other):
+      data_(std::move(other.data_)),
+      tail_(data_.beforeBegin())
+    {
+      rebuildTail();
+      other.tail_ = other.data_.beforeBegin();
+    }
 
-void push(T&& rhs)
-{
-  if (empty())
-  {
-    tail_ = data_.insertAfter(data_.beforeBegin(), std::move(rhs));
-  }
-  else
-  {
-    tail_ = data_.insertAfter(tail_, std::move(rhs));
-  }
-}
-T& front()
-{
-  if (empty())
-  {
-    throw std::out_of_range("Queue is empty");
-  }
-  return data_.front();
-}
+    Queue& operator=(const Queue& other)
+    {
+      if (this != &other)
+      {
+        data_ = other.data_;
+        rebuildTail();
+      }
+      return *this;
+    }
 
-const T& front() const
-{
-  if (empty())
-  {
-    throw std::out_of_range("Queue is empty");
-  }
-  return data_.front();
-}
-T drop()
-{
-  if (empty())
-  {
-    throw std::out_of_range("Queue is empty");
-  }
+    Queue& operator=(Queue&& other)
+    {
+      if (this != &other)
+      {
+        data_ = std::move(other.data_);
+        rebuildTail();
+        other.tail_ = other.data_.beforeBegin();
+      }
+      return *this;
+    }
 
-  bool wasLast = (data_.begin() == tail_);
-  T value = std::move(data_.front());
-  data_.pop_front();
+    ~Queue() = default;
 
-  if (wasLast)
-  {
-    tail_ = data_.beforeBegin();
-  }
+    bool empty() const noexcept
+    {
+      return data_.empty();
+    }
 
-  return value;
-}
+    void push(const T& rhs)
+    {
+      if (empty())
+      {
+        tail_ = data_.insertAfter(data_.beforeBegin(), rhs);
+      }
+      else
+      {
+        tail_ = data_.insertAfter(tail_, rhs);
+      }
+    }
+
+    void push(T&& rhs)
+    {
+      if (empty())
+      {
+        tail_ = data_.insertAfter(data_.beforeBegin(), std::move(rhs));
+      }
+      else
+      {
+        tail_ = data_.insertAfter(tail_, std::move(rhs));
+      }
+    }
+
+    T& front()
+    {
+      if (empty())
+      {
+        throw std::out_of_range("Queue is empty");
+      }
+      return data_.front();
+    }
+
+    const T& front() const
+    {
+      if (empty())
+      {
+        throw std::out_of_range("Queue is empty");
+      }
+      return data_.front();
+    }
+
+    T drop()
+    {
+      if (empty())
+      {
+        throw std::out_of_range("Queue is empty");
+      }
+
+      bool wasLast = (data_.begin() == tail_);
+      T value = std::move(data_.front());
+      data_.pop_front();
+
+      if (wasLast)
+      {
+        tail_ = data_.beforeBegin();
+      }
+
+      return value;
+    }
+
   private:
+    void rebuildTail()
+    {
+      tail_ = data_.beforeBegin();
+      for (auto it = data_.begin(); it != data_.end(); ++it)
+      {
+        tail_ = it;
+      }
+    }
+
     List< T > data_;
     Iterator< T > tail_;
   };
