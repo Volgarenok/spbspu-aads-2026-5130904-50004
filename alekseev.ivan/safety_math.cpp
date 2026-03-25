c295819de9078ffcf2b4f9625d00c69b760d14d9
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-03-25 20:10:29 +0300

    feat(safety_math): add mul

--- alekseev.ivan/safety_math.cpp
+++ alekseev.ivan/safety_math.cpp
@@ -29,3 +29,28 @@ int alekseev::sub(int a, int b)
   }
   return a - b;
 }
+
+int alekseev::mul(int a, int b)
+{
+  if (!a || !b) {
+    return 0;
+  }
+  if (a > 0 && b > 0) {
+    if (a > MAX / b) {
+      throw std::overflow_error("Mul overflow");
+    }
+  } else if (a < 0 && b < 0) {
+    if (a < MAX / b) {
+      throw std::overflow_error("Mul overflow");
+    }
+  } else if (a < 0 && b > 0) {
+    if (a < MIN / b) {
+      throw std::underflow_error("Mul underflow");
+    }
+  } else if (a > 0 && b < 0) {
+    if (b < MIN / a) {
+      throw std::underflow_error("Mul underflow");
+    }
+  }
+  return a * b;
+}
