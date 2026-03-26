de929431402535ea0a436f2299654598d384d286
Ausev.Arteam <seregabest77@gmail.com>
2026-03-27 00:15:15 +0300

    Добавил структуру списка с итераторами

--- aushev.arteam/S2/list.h
+++ aushev.arteam/S2/list.h
@@ -15,6 +15,36 @@ struct Node {
 template< class T >
 class List;
 
+template< class T >
+class LIter {
+  friend class List< T >;
+
+private:
+  Node< T >* node_;
+  List< T >* list_;
+};
+
+template< class T >
+class LCIter {
+  friend class List< T >;
+
+private:
+  const Node< T >* node_;
+  const List< T >* list_;
+};
+
+template< class T >
+class List {
+  friend class LIter< T >;
+  friend class LCIter< T >;
+
+private:
+  using NodeT = Node< T >;
+  NodeT* head_;
+  NodeT* tail_;
+  size_t size_;
+};
+
 }
 
 #endif
