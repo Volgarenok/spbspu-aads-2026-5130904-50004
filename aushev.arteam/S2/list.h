5a9ab4e141a03bd4321deee95d2422bf1aa0fc01
Ausev.Arteam <seregabest77@gmail.com>
2026-03-27 00:17:14 +0300

    Реализовал конструкторы и деструктор списка

--- aushev.arteam/S2/list.h
+++ aushev.arteam/S2/list.h
@@ -38,12 +38,83 @@ class List {
   friend class LIter< T >;
   friend class LCIter< T >;
 
+public:
+  List();
+  ~List();
+  List(const List& other);
+  List& operator=(const List& other);
+  List(List&& other) noexcept;
+  List& operator=(List&& other) noexcept;
+
 private:
   using NodeT = Node< T >;
   NodeT* head_;
   NodeT* tail_;
   size_t size_;
 };
+
+template< class T >
+List< T >::List()
+  : head_(nullptr)
+  , tail_(nullptr)
+  , size_(0)
+{
+}
+
+template< class T >
+List< T >::~List()
+{
+  clear();
+}
+
+template< class T >
+List< T >::List(const List& other)
+  : head_(nullptr)
+  , tail_(nullptr)
+  , size_(0)
+{
+  for (const auto& item : other) {
+    push_back(item);
+  }
+}
+
+template< class T >
+List< T >& List< T >::operator=(const List& other)
+{
+  if (this != &other) {
+    clear();
+    for (const auto& item : other) {
+      push_back(item);
+    }
+  }
+  return *this;
+}
+
+template< class T >
+List< T >::List(List&& other) noexcept
+  : head_(other.head_)
+  , tail_(other.tail_)
+  , size_(other.size_)
+{
+  other.head_ = nullptr;
+  other.tail_ = nullptr;
+  other.size_ = 0;
+}
+
+template< class T >
+List< T >& List< T >::operator=(List&& other) noexcept
+{
+  if (this != &other) {
+    clear();
+    head_ = other.head_;
+    tail_ = other.tail_;
+    size_ = other.size_;
+    other.head_ = nullptr;
+    other.tail_ = nullptr;
+    other.size_ = 0;
+  }
+  return *this;
+}
 
 }
 
