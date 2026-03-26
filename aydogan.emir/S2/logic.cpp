01c7c7244a59770e8e6a19368e0fa876018132c6
emiraydgn <emiraydogan03@gmail.com>
2026-03-26 08:17:26 +0300

    logic skeleton

---
+++ aydogan.emir/S2/logic.cpp
@@ -0,0 +1,18 @@
+#include "logic.hpp"
+
+namespace aydogan
+{
+  long long calculateExpression(const std::string& expression)
+  {
+    (void)expression;
+    return 0;
+  }
+
+  int run(std::istream& in, std::ostream& out, std::ostream& err)
+  {
+    (void)in;
+    (void)out;
+    (void)err;
+    return 0;
+  }
+}
