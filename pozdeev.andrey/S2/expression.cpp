def1bd1c1363429e86f327de85d16c8fea65a3a2
ryouta25 <avp22.07@mail.ru>
2026-03-28 01:34:05 +0300

    implement operator application and token parsing

--- pozdeev.andrey/S2/expression.cpp
+++ pozdeev.andrey/S2/expression.cpp
@@ -31,4 +31,58 @@ bool isOperator(const std::string &token)
         || token == "%" || token == "<<";
 }
 
+long long applyOperator(long long left, long long right, const std::string &op)
+{
+    if (op == "+") {
+        return left + right;
+    }
+    if (op == "-") {
+        return left - right;
+    }
+    if (op == "*") {
+        return left * right;
+    }
+    if (op == "/") {
+        if (right == 0) {
+            throw std::runtime_error("Division by zero");
+        }
+        return left / right;
+    }
+    if (op == "%") {
+        if (right == 0) {
+            throw std::runtime_error("Modulo by zero");
+        }
+        return left % right;
+    }
+    if (op == "<<") {
+        if (right < 0) {
+            throw std::runtime_error("Negative shift count");
+        }
+        return left << right;
+    }
+    throw std::runtime_error("Unknown operator");
+}
+
+Token parseToken(const std::string &str)
+{
+    Token token;
+    token.value_ = str;
+
+    if (str == "(") {
+        token.type_ = TokenType::LeftParen;
+    } else if (str == ")") {
+        token.type_ = TokenType::RightParen;
+    } else if (isOperator(str)) {
+        token.type_ = TokenType::Operator;
+    } else {
+        token.type_ = TokenType::Number;
+        try {
+            token.numberValue_ = std::stoll(str);
+        } catch (...) {
+            throw std::runtime_error("Invalid number: " + str);
+        }
+    }
+    return token;
+}
+
 }
