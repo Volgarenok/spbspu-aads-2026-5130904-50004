2bdea3f2cee026db20e2c912390e25b7190b34b3
EIKO7 <camper1773@mail.ru>
2026-04-08 16:31:42 +0300

    ver 6

--- ahrameev.vladislav/S2/expr.cpp
+++ ahrameev.vladislav/S2/expr.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdexcept>
 
 namespace akhrameev {
 
@@ -17,6 +18,29 @@ void tokenize(const std::string& line, Queue<std::string>& out) {
     }
 }
 
+void to_postfix(Queue<std::string>& tokens, Queue<std::string>& out) {
+    Stack<std::string> ops;
+    while (!tokens.empty()) {
+        std::string t = tokens.drop();
+
+        if (t == "(") {
+            ops.push(t);
+        } else if (t == ")") {
+            while (!ops.empty() && ops.top() != "(") {
+                out.push(ops.drop());
+            }
+            if (ops.empty()) throw std::runtime_error("Ошибка в скобках");
+            ops.drop(); 
+        } else {
+            out.push(t);
+        }
+    }
+    while (!ops.empty()) {
+        if (ops.top() == "(") throw std::runtime_error("Ошибка в скобках");
+        out.push(ops.drop());
+    }
+}
+
 int run(int argc, char* argv[]) {
     std::istream* input = &std::cin;
     std::ifstream file;
@@ -24,7 +48,7 @@ int run(int argc, char* argv[]) {
     if (argc > 1) {
         file.open(argv[1]);
         if (!file.is_open()) {
-            std::cerr << "Ошибка: не удалось открыть файл " << argv[1] << "\n";
+            std::cerr << "Ошибка: не удалось открыть файл " << argv[1] << std::endl;
             return 1;
         }
         input = &file;
@@ -34,13 +58,14 @@ int run(int argc, char* argv[]) {
     while (std::getline(*input, line)) {
         Queue<std::string> tokens;
         tokenize(line, tokens);
+
+        Queue<std::string> postfix;
+        to_postfix(tokens, postfix);
+
         
-        std::cout << "Tokens: ";
-        while (!tokens.empty()) {
-            std::cout << tokens.drop() << " ";
-        }
+        std::cout << "Postfix: ";
+        while (!postfix.empty()) std::cout << postfix.drop() << " ";
         std::cout << "\n";
     }
-    return 0;
-}
+  }
 }
