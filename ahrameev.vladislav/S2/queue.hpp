0f2eb1c43d524f1f266e46977beb2477e562e650
EIKO7 <camper1773@mail.ru>
2026-04-08 15:55:20 +0300

    ver 3: adding a queue template

--- ahrameev.vladislav/S2/queue.hpp
+++ ahrameev.vladislav/S2/queue.hpp
@@ -1,17 +1,50 @@
 #ifndef QUEUE_HPP
 #define QUEUE_HPP
 
+#include <stdexcept>
+
 namespace akhrameev {
     template <typename T>
     class Queue {
+        struct Node {
+            T data;
+            Node* next;
+            Node(T val, Node* n) : data(val), next(n) {}
+        };
+        Node* head = nullptr;
+        Node* tail = nullptr;
+
     public:
         Queue() = default;
         Queue(const Queue&) = delete;
         Queue& operator=(const Queue&) = delete;
-        ~Queue() {}
-        void push(T rhs) {}
-        T drop() { return T(); }
-        bool empty() const { return true; }
+
+        ~Queue() {
+            while (head) {
+                Node* temp = head;
+                head = head->next;
+                delete temp;
+            }
+        }
+
+        void push(T rhs) {
+            Node* new_node = new Node(rhs, nullptr);
+            if (tail) tail->next = new_node;
+            else head = new_node;
+            tail = new_node;
+        }
+
+        T drop() {
+            if (!head) throw std::runtime_error("Очередь пуста");
+            T val = head->data;
+            Node* temp = head;
+            head = head->next;
+            if (!head) tail = nullptr;
+            delete temp;
+            return val;
+        }
+
+        bool empty() const { return head == nullptr; }
     };
 }
 #endif
