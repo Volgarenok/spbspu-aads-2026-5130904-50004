3305f50580955a911b60893b8357a2abcca40d1f
BinaryKenny <khairullinvadim2006@gmail.com>
2026-05-10 16:54:13 +0300

    The initial commit for tests

--- khairullin.vadim/S3/Test.cpp
+++ khairullin.vadim/S3/Test.cpp
@@ -1,3 +1,13 @@
-//
-// Created by Vadim Khairullin on 5/10/26.
-//
+#if defined(__GNUC__) || defined(__clang__)
+__attribute__((weak)) int main(int argc, char* argv[]);
+#endif
+
+#define BOOST_TEST_MODULE S2
+#include <boost/test/included/unit_test.hpp>
+#include "GraphSystem.h"
+
+BOOST_AUTO_TEST_SUITE(main_test)
+{
+  khairullin::HashTable hashTable;
+  BOOST_TEST(table);
+}
