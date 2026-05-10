5fbfe31f8e1963d90c6f63d4443fe072df96764d
ekaterinaivanc <kataivancova7@gmail.com>
2026-05-10 16:25:41 +0300

    Add addEdge and removeEdge

--- ivantsova.ekaterina/S3/graph.cpp
+++ ivantsova.ekaterina/S3/graph.cpp
@@ -27,3 +27,40 @@ bool ivantsova::Graph::hasVertex(const std::string& name) const {
 ivantsova::Vector<std::string> ivantsova::Graph::getVertices() const {
   return vertices_;
 }
+
+void ivantsova::Graph::addEdge(const std::string& from, const std::string& to, unsigned long long weight) {
+  addVertex(from);
+  addVertex(to);
+  EdgeKey key(from, to);
+  if (edges_.has(key)) {
+    edges_.get(key).pushBack(weight);
+  } else {
+    Vector<unsigned long long> weights;
+    weights.pushBack(weight);
+    edges_.add(key, weights);
+  }
+}
+
+bool ivantsova::Graph::removeEdge(const std::string& from, const std::string& to, unsigned long long weight) {
+  if (!hasVertex(from) || !hasVertex(to)) {
+    return false;
+  }
+  EdgeKey key(from, to);
+  if (!edges_.has(key)) {
+    return false;
+  }
+  Vector<unsigned long long>& weights = edges_.get(key);
+  for (size_t i = 0; i < weights.getSize(); ++i) {
+    if (weights[i] == weight) {
+      for (size_t j = i; j < weights.getSize() - 1; ++j) {
+        weights[j] = weights[j + 1];
+      }
+      weights.popBack();
+      if (weights.getSize() == 0) {
+        edges_.drop(key);
+      }
+      return true;
+    }
+  }
+  return false;
+}
