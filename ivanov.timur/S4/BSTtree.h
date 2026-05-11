d6a50911eb289bd76615f222d8fd74493a2f24fa
isaacgreenfield <ivtim708@gmail.com>
2026-05-11 19:10:58 +0300

    Added drop, it probably works

--- ivanov.timur/S4/BSTtree.h
+++ ivanov.timur/S4/BSTtree.h
@@ -61,7 +61,31 @@ public:
     throw std::out_of_range("No such element here");
   }
   Value drop(Key k) {
+    BSTtree *curr = left;
+    while (curr != &nil) {
+      if (comp(k, curr->key)) curr = curr->left;
+      else if (comp(curr->key, k)) curr = curr->right;
+      else break;
+    }
+    if (curr == &nil) throw std::out_of_range("No such element here");
 
+    Value rmv = curr->val;
+
+    if (curr->left != &nil && curr->right != &nil) {
+      BSTtree *mRight = curr->right;
+      while (mRight->left != &nil) mRight = mRight->left;
+      curr->val = mRight->val;
+      curr = mRight;
+      rmv = curr->val;
+    }
+
+    BSTtree *child = (curr->left != &nil) ? curr->left : curr->right;
+    child->parent = curr->parent;
+    if (curr->parent->left == curr) curr->parent->left = child;
+    else curr->parent->right = child;
+
+    delete curr;
+    return rmv;
   }
 
   using const_iterator = BSTConstIterator<Key, Value>;
