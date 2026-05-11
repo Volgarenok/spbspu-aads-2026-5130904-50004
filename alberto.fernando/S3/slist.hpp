02f89d4bbc05ed11af001bbd243ab5022faf921e
Fernando Diaz <fernandodiasfd780518@gmail.com>
2026-05-11 08:13:46 -0300

    Insertion and removal methods

--- alberto.fernando/S3/slist.hpp
+++ alberto.fernando/S3/slist.hpp
@@ -58,3 +58,70 @@ public:
     std::swap(head_, o.head_);
     std::swap(size_, o.size_);
   }
+  void push_front(const T& v)
+  {
+    Node* n = new Node(v);
+    n->next = head_;
+    head_   = n;
+    ++size_;
+  }
+
+  void push_front(T&& v)
+  {
+    Node* n = new Node(std::move(v));
+    n->next = head_;
+    head_   = n;
+    ++size_;
+  }
+
+  void push_back(const T& v)
+  {
+    Node* n = new Node(v);
+    if (!head_) {
+      head_ = n;
+    } else {
+      Node* cur = head_;
+      while (cur->next) {
+        cur = cur->next;
+      }
+      cur->next = n;
+    }
+    ++size_;
+  }
+
+  template< class Pred >
+  bool remove_if(Pred pred)
+  {
+    Node** cur = &head_;
+    while (*cur) {
+      if (pred((*cur)->data)) {
+        Node* del = *cur;
+        *cur = del->next;
+        delete del;
+        --size_;
+        return true;
+      }
+      cur = &(*cur)->next;
+    }
+    return false;
+  }
+
+  void clear()
+  {
+    while (head_) {
+      Node* n = head_->next;
+      delete head_;
+      head_ = n;
+    }
+    size_ = 0;
+  }
+
+  size_t size() const
+  {
+    return size_;
+  }
+
+  bool empty() const
+  {
+    return size_ == 0;
+  }
