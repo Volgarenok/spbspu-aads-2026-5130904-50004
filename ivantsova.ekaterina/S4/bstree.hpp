4a886497a35904242502d8c2e64f27001e545aae
ekaterinaivanc <kataivancova7@gmail.com>
2026-05-13 18:32:55 +0300

    Add constructor and destructor, include cstddef

--- ivantsova.ekaterina/S4/bstree.hpp
+++ ivantsova.ekaterina/S4/bstree.hpp
@@ -2,6 +2,7 @@
 #define BSTREE_HPP
 
 #include <functional>
+#include <cstddef>
 #include "bstiterators.hpp"
 #include "treenode.hpp"
 
@@ -60,6 +61,33 @@ namespace ivantsova {
     size_t calcHeight(const Node *node) const;
     void removeNode(Node *node);
   };
+}
+
+template < class Key, class Value, class Compare >
+ivantsova::BSTree< Key, Value, Compare >::BSTree() :
+  fake_root_(new Node(Key(), Value())),
+  size_(0),
+  comp_()
+{
+  fake_root_->left_ = nullptr;
+  fake_root_->right_ = nullptr;
+  fake_root_->parent_ = nullptr;
+}
+
+template < class Key, class Value, class Compare >
+ivantsova::BSTree< Key, Value, Compare >::~BSTree() {
+  clear();
+  delete fake_root_;
+}
+
+template < class Key, class Value, class Compare >
+bool ivantsova::BSTree< Key, Value, Compare >::empty() const {
+  return size_ == 0;
+}
+
+template < class Key, class Value, class Compare >
+size_t ivantsova::BSTree< Key, Value, Compare >::size() const {
+  return size_;
 }
 
 #endif
