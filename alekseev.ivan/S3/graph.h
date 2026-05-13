5441164f642efb882e71fa0762b057f009d9c097
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-13 15:15:18 +0300

    feat(graph): add Ro5 and swap

--- alekseev.ivan/S3/graph.h
+++ alekseev.ivan/S3/graph.h
@@ -5,16 +5,25 @@
 #include <boost/uuid/sha1.hpp>
 
 namespace alekseev {
-  size_t hasher(std::pair< std::string, std::string > key);
-  bool is_equal(std::pair< std::string, std::string > lhs,
-      std::pair< std::string, std::string > rhs);
+  size_t hasher(const std::pair< std::string, std::string > & key);
+  bool is_equal(const std::pair< std::string, std::string > & lhs,
+      const std::pair< std::string, std::string > & rhs);
 
   struct Graph {
+    ~Graph();
+    Graph(const Graph & rhs);
+    Graph & operator=(const Graph & rhs);
+    Graph(Graph && rhs) noexcept;
+    Graph & operator=(Graph && rhs) noexcept;
+
+    void swap(Graph & rhs) noexcept;
+
     private:
-      HashTable< std::pair< std::string, std::string >, List< size_t >, hasher, is_equal > graph;
+      List< std::string > * vertexes_;
+      HashTable< std::pair< std::string, std::string >, List< size_t >, hasher, is_equal > edges_;
   };
 
-  size_t hasher(std::pair< std::string, std::string > key)
+  size_t hasher(const std::pair< std::string, std::string > & key)
   {
     boost::uuids::detail::sha1 first;
     first.process_bytes(key.first.c_str(), key.first.size());
@@ -33,11 +42,51 @@ namespace alekseev {
     return result;
   }
 
-  bool is_equal(std::pair< std::string, std::string > lhs,
-      std::pair< std::string, std::string > rhs)
+  bool is_equal(const std::pair< std::string, std::string > & lhs,
+      const std::pair< std::string, std::string > & rhs)
   {
     return lhs.first == rhs.first && lhs.second == rhs.second || lhs.first == rhs.second && lhs.
         second == rhs.first;
+  }
+
+  Graph::~Graph()
+  {
+    if (vertexes_) {
+      clear(vertexes_->next, vertexes_);
+      rmfake(vertexes_);
+    }
+  }
+
+  Graph::Graph(const Graph & rhs):
+    vertexes_(deep_copy(rhs.vertexes_)),
+    edges_(rhs.edges_)
+  {
+  }
+
+  Graph & Graph::operator=(const Graph & rhs)
+  {
+    Graph temp(rhs);
+    swap(temp);
+    return *this;
+  }
+
+  Graph::Graph(Graph && rhs) noexcept:
+    vertexes_(rhs.vertexes_),
+    edges_(std::move(rhs.edges_))
+  {
+    rhs.vertexes_ = nullptr;
+  }
+
+  inline Graph & Graph::operator=(Graph && rhs) noexcept
+  {
+    swap(rhs);
+    return *this;
+  }
+
+  inline void Graph::swap(Graph & rhs) noexcept
+  {
+    std::swap(vertexes_, rhs.vertexes_);
+    edges_.swap(rhs.edges_);
   }
 }
 
