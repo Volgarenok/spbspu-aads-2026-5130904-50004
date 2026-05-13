cb3555f39e906ef99c134f9381c0026ff594eec6
SoundwaveAI <ghost10.town10000@gmail.com>
2026-05-13 18:28:25 +0300

    create realization for hashtable and hiter

--- kuchukbaeva.alyona/S3/hashTable.hpp
+++ kuchukbaeva.alyona/S3/hashTable.hpp
@@ -115,4 +115,157 @@ namespace kuchukbaeva {
     size_t massIdx_;
     LIter< std::pair< Key, Value > > listIt_;
   };
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< Key, Value, Hash, Equal >::HashTable(size_t slots) :
+    mass_(slots, List< std::pair< Key, Value > >()),
+    size_(0),
+    hashFn_(),
+    equalFn_()
+  {
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< Key, Value, Hash, Equal >::HashTable(const HashTable& other) :
+    mass_(other.mass_.getSize(), List< std::pair< Key, Value > >()),
+    size_(0),
+    hashFn_(other.hashFn_),
+    equalFn_(other.equalFn_)
+  {
+    for (size_t i = 0; i < other.mass_.getSize(); ++i) {
+      for (auto it = other.mass_[i].cbegin(); it != other.mass_[i].cend(); ++it) {
+        this->add(it->first, it->second);
+      }
+    }
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< Key, Value, Hash, Equal >::HashTable(HashTable&& other) noexcept :
+    HashTable(1)
+  {
+    swap(other);
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< Key, Value, Hash, Equal >& HashTable< Key, Value, Hash, Equal >::operator=(const HashTable& other)
+  {
+    if (this != &other) {
+      HashTable tmp(other);
+      swap(tmp);
+    }
+    return *this;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HashTable< Key, Value, Hash, Equal >& HashTable< Key, Value, Hash, Equal >::operator=(HashTable&& other) noexcept
+  {
+    if (this != &other) {
+      HashTable tmp(std::move(other));
+      swap(tmp);
+    }
+    return *this;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  void HashTable< Key, Value, Hash, Equal >::swap(HashTable& other) noexcept
+  {
+    mass_.swap(other.mass_);
+    std::swap(size_, other.size_);
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  void HashTable< Key, Value, Hash, Equal >::add(const Key& k, const Value& v)
+  {
+    size_t idx = hashFn_(k) % mass_.getSize();
+    for (auto it = mass_[idx].begin(); it != mass_[idx].end(); ++it) {
+      if (equalFn_(it->first, k)) {
+        it->second = v;
+        return;
+      }
+    }
+    mass_[idx].push_front(std::make_pair(k, v));
+    ++size_;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  bool HashTable< Key, Value, Hash, Equal >::drop(const Key& k)
+  {
+    size_t idx = hashFn_(k) % mass_.getSize();
+    auto beforeIt = mass_[idx].beforeBegin();
+    auto it = mass_[idx].begin();
+    while (it != mass_[idx].end()) {
+      if (equalFn_(it->first, k)) {
+        mass_[idx].eraseAfter(beforeIt);
+        --size_;
+        return true;
+      }
+      ++it;
+      ++beforeIt;
+    }
+    return false;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  bool HashTable< Key, Value, Hash, Equal >::has(const Key& k) const
+  {
+    size_t idx = hashFn_(k) % mass_.getSize();
+    for (auto it = mass_[idx].cbegin(); it != mass_[idx].cend(); ++it) {
+      if (equalFn_(it->first, k)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  Value* HashTable< Key, Value, Hash, Equal >::find(const Key& k)
+  {
+    size_t idx = hashFn_(k) % mass_.getSize();
+    for (auto it = mass_[idx].begin(); it != mass_[idx].end(); ++it) {
+      if (equalFn_(it->first, k)) {
+        return &(it->second);
+      }
+    }
+    return nullptr;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  void HashTable< Key, Value, Hash, Equal >::rehash(size_t slots)
+  {
+    if (slots == 0) {
+      slots = 1;
+    }
+    HashTable tmp(slots);
+    for (size_t i = 0; i < mass_.getSize(); ++i) {
+      for (auto it = mass_[i].begin(); it != mass_[i].end(); ++it) {
+        tmp.add(it->first, it->second);
+      }
+    }
+    swap(tmp);
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  size_t HashTable< Key, Value, Hash, Equal >::getSize() const
+  {
+    return size_;
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HTIter< Key, Value, Hash, Equal > HashTable< Key, Value, Hash, Equal >::begin()
+  {
+    for (size_t i = 0; i < mass_.getSize(); ++i) {
+      if (!mass_[i].isEmpty()) {
+        return HTIter< Key, Value, Hash, Equal >(&mass_, i, mass_[i].begin());
+      }
+    }
+    return end();
+  }
+
+  template< class Key, class Value, class Hash, class Equal >
+  HTIter< Key, Value, Hash, Equal > HashTable< Key, Value, Hash, Equal >::end()
+  {
+    return HTIter< Key, Value, Hash, Equal >(&mass_, mass_.getSize(), LIter< std::pair< Key, Value > >());
+  }
+
 }
+
+#endif
