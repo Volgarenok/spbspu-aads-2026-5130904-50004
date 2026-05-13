5f571e4c6c61c396e9d2c49168d847ac7a3f6b60
isaacgreenfield <ivtim708@gmail.com>
2026-05-13 19:09:48 +0300

    added load from file

--- ivanov.timur/S4/BSTManager.h
+++ ivanov.timur/S4/BSTManager.h
@@ -1,23 +1,54 @@
 #ifndef BSTMANAGER_H
 #define BSTMANAGER_H
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include "BSTtree.h"
 
 
 class BSTManager {
+  using tree = BSTtree<int, std::string, std::less<int>>;
 public:
   void loadFromFile(const std::string& filename);
   void execute(const std::string& line, bool& isAnything, bool silent);
 
 private:
-  std::vector<std::pair<std::string,
-    BSTtree<int, std::string, std::less<int>>>> datasets;
+  std::vector<std::pair<std::string, tree>> datasets;
 
-  BSTtree<int, std::string, std::less<int>>* getDataset(const std::string& name);
-  const BSTtree<int, std::string, std::less<int>>* getDatasetConst(const std::string& name) const;
+  tree* getDataset(const std::string& name);
+  const tree* getDatasetConst(const std::string& name) const;
 
-  bool contains(const BSTtree<int, std::string, std::less<int>>& tree, int key) const;
+  bool contains(const tree& tr, int key) const;
 };
+
+inline void BSTManager::loadFromFile(const std::string &filename) {
+  std::ifstream file(filename);
+  if (!file.is_open()) {
+    std::cerr << "File opening error";
+    std::exit(1);
+  }
+
+  std::string line;
+  while (std::getline(file, line)) {
+    if (line.empty()) continue;
+    std::istringstream iss(line);
+    std::string dname;
+    if (!(iss >> dname)) continue;
+
+    tree* tr = getDataset(dname);
+    if (!tr) {
+      datasets.push_back(std::make_pair(dname, tree()));
+      tr = &datasets.back().second;
+    }
+
+    int key;
+    std::string val;
+    while (iss >> key >> val) {
+      tr->push(key, val);
+    }
+  }
+}
 
 #endif
