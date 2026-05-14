868f6ec9b9b371f420102db712250fe98ad5f318
alekseev-ii <alekseev.ii@edu.spbstu.ru>
2026-05-15 02:09:12 +0300

    feat(main): remove bad declarations

--- alekseev.ivan/S3/main.cpp
+++ alekseev.ivan/S3/main.cpp
@@ -6,34 +6,18 @@
 
 namespace alekseev {
   using str = std::string;
-  size_t graph_name_hasher(const str & name);
+  size_t str_hasher(const str & name);
   Graph input_graph(std::ifstream & input);
   bool str_less(str a, str b);
 
-  std::ostream & graphs(std::ostream & output, Vector< str > names);
-  std::ostream & vertexes(std::ostream & output, List< str > * names);
-  std::ostream & outbounds(std::ostream & output,
-      Vector< std::pair< str, Vector< size_t > > > edges);
-  std::ostream & inbounds(std::ostream & output,
-      Vector< std::pair< str, Vector< size_t > > > * edges);
-  void bound(Graph & graph, const str & vertex1, const str & vertex2, size_t weight);
-  void cut(Graph & graph, const str & vertex1, const str & vertex2, size_t weight);
-  void create(
-      HashTable< str, Graph, size_t(*)(const str &), bool (*)(const str &, const str &) > & ht,
-      const str & name);
-  void merge(
-      HashTable< str, Graph, size_t(*)(const str &), bool (*)(const str &, const str &) > & ht,
-      const str & new_name, const str & name1, const str & name2);
-  void extract(
-      HashTable< str, Graph, size_t(*)(const str &), bool (*)(const str &, const str &) > & ht,
-      const str & new_name, const str & name, Vector< str > * vertexes);
+
 }
 
 int main()
 {
 }
 
-size_t alekseev::graph_name_hasher(const str & name)
+size_t alekseev::str_hasher(const str & name)
 {
   boost::uuids::detail::sha1 h;
   h.process_bytes(name.c_str(), name.size());
