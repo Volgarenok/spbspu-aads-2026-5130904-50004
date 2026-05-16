658c2d93c6b16ac2afd18c827e6fe4ec167aebef
FShaaM <zinovevas135@gmail.com>
2026-05-16 23:44:12 +0300

    Begining main()

--- zinoviev.alexander/S4/main.cpp
+++ zinoviev.alexander/S4/main.cpp
@@ -1,2 +1,32 @@
-int main()
-{}
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "BSTree.hpp"
+
+int main(int argc, const char** argv)
+{
+    if (argc < 2)
+    {
+        std::cout << "No filename provided\n";
+        return 1;
+    }
+
+    std::ifstream file(argv[1]);
+
+    if (!file.is_open())
+    {
+        std::cout << "Can't open file\n";
+        return 1;
+    }
+
+
+
+    std::string line;
+    while (std::getline(file, line))
+    {
+        if (line.empty())
+            continue;
+
+
+    }
+}
