d4af1fbbad3117166bb66f719fb78826f3472ba7
FShaaM <zinovevas135@gmail.com>
2026-05-16 19:50:17 +0300

    Added CIterator

--- zinoviev.alexander/S4/bstree.hpp
+++ zinoviev.alexander/S4/bstree.hpp
@@ -29,7 +29,61 @@ namespace zinoviev
     size_t size_;
     Compare compare_;
 
-    Node* next(Node* cur);
+    Node* next(Node* cur)
+    {
+      if (cur->right_)
+      {
+        cur = cur->right_;
+
+        while (cur->left_)
+          cur = cur->left_;
+      }
+      else
+      {
+        Node* parent = cur->parent_;
+
+        while (parent && cur != parent->left_)
+        {
+          cur = parent;
+          parent = parent->parent_;
+        }
+
+        if (!parent)
+          cur = nullptr;
+        else
+          cur = parent;
+      }
+
+      return cur;
+    }
+
+    const Node* next(const Node* cur)
+    {
+      if (cur->right_)
+      {
+        cur = cur->right_;
+
+        while (cur->left_)
+          cur = cur->left_;
+      }
+      else
+      {
+        Node* parent = cur->parent_;
+
+        while (parent && cur != parent->left_)
+        {
+          cur = parent;
+          parent = parent->parent_;
+        }
+
+        if (!parent)
+          cur = nullptr;
+        else
+          cur = parent;
+      }
+
+      return cur;
+    }
 
   public:
     class Iterator
@@ -78,6 +132,46 @@ namespace zinoviev
 
     class CIterator
     {
+      const Node* current_;
+    public:
+
+      CIterator(const Node* cur) :
+        current_(cur)
+      {
+      }
+
+      const std::pair<const Key, Value>& operator*()
+      {
+        return current_->data_;
+      }
+
+      const std::pair<const Key, Value>* operator->()
+      {
+        return &(current_->data_);
+      }
+
+      CIterator& operator++()
+      {
+        current_ = next(current_);
+        return *this;
+      }
+
+      CIterator operator++(int)
+      {
+        CIterator tmp = CIterator(current_);
+        current_ = next(current_);
+        return tmp;
+      }
+
+      bool operator==(const CIterator& other) const
+      {
+        return this->current_ == other.current_;
+      }
+
+      bool operator!=(const CIterator& other) const
+      {
+        return !(*this == other);
+      }
     };
 
     BSTree(const Compare& com = Compare()) :
@@ -86,35 +180,35 @@ namespace zinoviev
       compare_(com)
     {
     }
+
+    Iterator begin();
+    Iterator end();
+    CIterator cbegin();
+    CIterator cend();
   };
 
   template <class Key, class Value, class Compare>
-  typename BSTree<Key, Value, Compare>::Node* BSTree<Key, Value, Compare>::next(Node* cur)
+  typename BSTree<Key, Value, Compare>::Iterator BSTree<Key, Value, Compare>::begin()
   {
-    if (cur->right_)
-    {
-      cur = cur->right_;
+    return Iterator{ root_ };
+  }
 
-      while (cur->left_)
-        cur = cur->left_;
-    }
-    else
-    {
-      Node* parent = cur->parent_;
+  template <class Key, class Value, class Compare>
+  typename BSTree<Key, Value, Compare>::Iterator BSTree<Key, Value, Compare>::end()
+  {
+    return Iterator{ nullptr };
+  }
 
-      while (parent && cur != parent->left_)
-      {
-        cur = parent;
-        parent = parent->parent_;
-      }
+  template <class Key, class Value, class Compare>
+  typename BSTree<Key, Value, Compare>::CIterator BSTree<Key, Value, Compare>::cbegin()
+  {
+    return CIterator{ root_ };
+  }
 
-      if (!parent)
-        cur = nullptr;
-      else
-        cur = parent;
-    }
-
-    return cur;
+  template <class Key, class Value, class Compare>
+  typename BSTree<Key, Value, Compare>::CIterator BSTree<Key, Value, Compare>::cend()
+  {
+    return Iterator{ nullptr };
   }
 }
 #endif
