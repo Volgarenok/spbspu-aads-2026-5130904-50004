452c2df8eee52100427972cbc4e326215839417e
isaacgreenfield <ivtim708@gmail.com>
2026-05-17 20:08:27 +0300

    Started to actually work on IndexHandler

--- ivanov.timur/F0/IndexHandler.h
+++ ivanov.timur/F0/IndexHandler.h
@@ -1,9 +1,24 @@
 #ifndef INDEXHANDLER_H
 #define INDEXHANDLER_H
+#include "Index.h"
+#include "RBtree.h"
 
 class IndexHandler {
 private:
+  ivanov::RBtree<std::string, Index*> indexesTree_;
 
+  Index* findIndex(const std::string& name) const {
+    Index** ptr = indexesTree_.search(name);
+    return ptr ? *ptr : nullptr;
+  }
+
+  void addIndex(const std::string& name, Index* newIdx) {
+    if (findIndex(name)) {
+      indexesTree_.remove(name);
+      delete findIndex(name);
+    }
+    indexesTree_.insert(name, newIdx);
+  }
 public:
 
 };
