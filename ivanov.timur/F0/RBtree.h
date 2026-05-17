04af41d3d3da7231ef3ad1946b9b9451b9a8cc94
isaacgreenfield <ivtim708@gmail.com>
2026-05-17 13:44:20 +0300

    Started constructing RBtree

--- ivanov.timur/F0/RBtree.h
+++ ivanov.timur/F0/RBtree.h
@@ -1,9 +1,24 @@
 #ifndef RBTREE_H
 #define RBTREE_H
 
+template <class Key, class Value>
 class RBtree {
 private:
 
+  struct Node {
+    Key key;
+    Value val;
+    bool color; //red - true, black - false
+    Node *left;
+    Node *right;
+    Node *parent;
+
+    Node(const Key& k, const Value& v, bool c = true)
+        : key(k), val(v), color(c), left(nullptr), right(nullptr), parent(nullptr) {}
+  };
+
+  Node *root;
+  Node *NIL;
 public:
 };
 
