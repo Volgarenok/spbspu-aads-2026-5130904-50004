f1d1f1e7fa312a24e40c4ec0c3af6956e754f943
isaacgreenfield <ivtim708@gmail.com>
2026-05-17 19:47:14 +0300

    Added clear + building from file

--- ivanov.timur/F0/Index.h
+++ ivanov.timur/F0/Index.h
@@ -2,6 +2,8 @@
 #define INDEX_H
 #include "RBtree.h"
 
+#include <fstream>
+#include <sstream>
 #include <vector>
 
 class Index {
@@ -21,6 +23,44 @@ private:
   }
 
 public:
+  Index() : canReconstruct_(false), totalWords_(0) {}
+
+  bool buildFromFile(const std::string& filename) {
+    std::ifstream file(filename);
+    if (!file.is_open()) return false;
+    clear();
+
+    std::string line, word;
+    int pos = 0;
+    canReconstruct_ = true;
+
+    while (std::getline(file, line)) {
+      std::istringstream iss(line);
+      while (iss >> word) {
+        word = normalize_(word);
+        if (word.empty()) continue;
+
+        wordOrder_.push_back(word);
+
+        auto* positions = invIndex_.search(word);
+        if (positions) {
+          positions->push_back(pos);
+        } else {
+          invIndex_.insert(word, std::vector<int>{pos});
+        }
+        ++pos;
+      }
+    }
+    totalWords_ = pos;
+    return true;
+  }
+
+  void clear() {
+    invIndex_.clear();
+    wordOrder_.clear();
+    canReconstruct_ = false;
+    totalWords_ = 0;
+  }
 };
 
 #endif
