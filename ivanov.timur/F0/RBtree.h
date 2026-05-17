8835033e138860cab0e880397393894276670378
isaacgreenfield <ivtim708@gmail.com>
2026-05-17 19:07:32 +0300

    Added autorotation

--- ivanov.timur/F0/RBtree.h
+++ ivanov.timur/F0/RBtree.h
@@ -8,7 +8,7 @@ private:
   struct Node {
     Key key;
     Value val;
-    bool color; //red - true, black - false
+    bool color; //true - true, false - false
     Node *left;
     Node *right;
     Node *parent;
@@ -55,6 +55,48 @@ private:
     y->right = x;
     x->parent = y;
   }
+  void insertFixUp(Node *z) {
+    while (z->parent->colour == true) {
+      if (z->parent == z->parent->parent->left) {   // родитель — левый
+        Node *y = z->parent->parent->right;
+        //дядя красный
+        if (y->colour == true) {
+          z->parent->colour = false;
+          y->colour = false;
+          z->parent->parent->colour = true;
+          z = z->parent->parent;
+        } else {
+          //дядя чёрный, z — правый потомок
+          if (z == z->parent->right) {
+            z = z->parent;
+            leftRotate(z);
+          }
+          //дядя чёрный, z — левый потомок
+          z->parent->colour = false;
+          z->parent->parent->colour = true;
+          rightRotate(z->parent->parent);
+        }
+      } else { // родитель — правый
+        Node *y = z->parent->parent->left;
+        if (y->colour == true) {
+          z->parent->colour = false;
+          y->colour = false;
+          z->parent->parent->colour = true;
+          z = z->parent->parent;
+        } else {
+          if (z == z->parent->left) {
+            z = z->parent;
+            rightRotate(z);
+          }
+          z->parent->colour = false;
+          z->parent->parent->colour = true;
+          leftRotate(z->parent->parent);
+        }
+      }
+    }
+    root->colour = false;
+  }
+
 public:
 };
 
